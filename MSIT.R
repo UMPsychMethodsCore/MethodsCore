0bb454e61fb1f5fb83d8eb8c77acd77d7e91e799
kesslerd <dankessler@gmail.com>
2011-10-12 15:19:33 -0400

    Introduced MSIT.R script, which does MSIT-specific task handling (this is however still incomplete)

---
+++ MSIT.R
@@ -0,0 +1,25 @@
+
+RespCalc=function(stimRESP,jitRESP,srcind){
+  resp=NA
+  src=''
+  flag=0
+  stimRESP=as.numeric(stimRESP)
+  jitRESP=as.numeric(jitRESP)
+  if (!is.na(stimRESP)){resp=stimRESP;src='stim'} else if(!is.na(jitRESP)) {resp=jitRESP;src='jit'}
+  if(srcind=='resp'){return(resp)}
+  if(srcind=='src'){return(src)}
+}
+
+data=within(data,{   #Calculate accuracy and trial duration within the data environment (makes the code much easier to read)
+  Resp=mapply(RespCalc,get(opts$MSIT$StimRespField),get(opts$MSIT$JitterRespField),'resp')
+  RespSrc=mapply(RespCalc,get(opts$MSIT$StimRespField),get(opts$MSIT$JitterRespField),'src')
+  RespRecode=Resp-1
+  CResp=get(opts$MSIT$CorrectResponseField)
+  Acc=ifelse(data$Resp==get(opts$MSIT$CorrectResponseField) & !is.na(data$Resp),1,0)
+  TrialDur=ifelse(RespSrc=='stim',get(opts$MSIT$StimRTfield),NA)
+  TrialDur=ifelse(RespSrc=='jit',get(opts$MSIT$JitterRTfield),TrialDur)
+  TrialDur=TrialDur/1000
+})
+  
+
+
