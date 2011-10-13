dfae375fd58ce18bf671a28ec8b4596cc42bb540
Daniel A. Kessler <kesslerd@umich.edu>
2011-10-13 14:31:45 -0400

    First skeleton of svmbatch.sh written
    Fixed file references to include '+orig'. Successful test run through on garbage data
    
    Updated readme file

--- svmbatch/svmbatch.sh
+++ svmbatch/svmbatch.sh
@@ -1,24 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
-#List of some useful variables
-
-
+ 
+#Read in variable values from associated files
 filelist1=`cat filelist1`
 filelist2=`cat filelist2`
 svmdir=`cat svmdir`
 
-
-
+#Build bucket files out of each of your conditions
 3dbucket -sessiondir $svmdir -prefixname bucket1 $filelist1
 3dbucket -sessiondir $svmdir -prefixname bucket2 $filelist2
 
+#Change to the svm working directory (presumably where this script was called from)
 cd $svmdir
 
-3dbucket -prefixname bucket bucket1 bucket2
+#Combine your two class bucket files into one super bucket
+3dbucket -prefixname bucket bucket1+orig bucket2+orig
 
-3dcalc -a bucket -expr "a" -prefixname shortbucket -datum short
+#Convert your bucket file to shorts
+3dcalc -a bucket+orig -expr "a" -prefixname shortbucket -datum short
 
-3dTcat -prefix timeshortbucket shortbucket
+#Convert your short bucket to be of type "time" (required by 3dsvm)
+3dTcat -prefix timeshortbucket shortbucket+orig
+
+#Build filelist by appending 1s or 2s to label file based on number in each class category
 
 for i in $filelist1
 do
@@ -30,7 +34,14 @@ do
 echo 2 >> labels.1D
 done
 
-3dAutomask timeshortbucket
+#Build a mask (in the future we may want to learn more about these options, or allow for specification of a custom mask
+3dAutomask timeshortbucket+orig
 
-3dsvm -trainvol timeshortbucket -trainlabels labels.1D -mask automask -model model -bucket bucket
+#Run your 3dsvm model
+3dsvm -trainvol timeshortbucket+orig -trainlabels labels.1D -mask automask+orig -model model -bucket weightbucket
 
+
+##To add:
+#1) Model testing (on training data itself, be sure to use set detrend to no
+#2) Ability to automatically split examples into training and test set, and do cross validation randomly
+#3) Permutation tests (basically just a for loop with random permutation of labels file. Tricky thing is querying test model weights against giant population of permutation weights
