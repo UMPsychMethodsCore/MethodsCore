976e602dacbe5330307132f20acf743d6b0d7e97
Shijia Guo <guosj@umich.edu>
2012-01-26 12:36:04 -0500

    permutation function done; main mfile is done using permutation function

--- permutation/permu.m
+++ permutation/permu.m
@@ -1,4 +1,6 @@
 %%
+t=cputime;
+
 %define nodes
 nnodes=5;       %# of nodes
 
@@ -11,40 +13,7 @@ DrivVec=[2,1,0,0,1]';
 %---------    define driving permutation vector   -------
 %---------           defined as 'DrivPermu'         -----
 %implement permutation(get every possible permutation)
-%%%%%%%%%%%%%%%%%%%%%%FROM HERE BASICLY THE SAME%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%CAN BE IMPLEMENTED AS A FUNCTION%%%%%%%%%%%
-%!!!!!!!etract must be's
-DrivVecPermuDef=(DrivVec==2);    
-
-%list all situation for could be's 
-DrivVecPos=(DrivVec==1);    %extract could be's
-nposs=nnz(DrivVecPos);  %# of 1s
-DrivPermuMask=str2vec(dec2bin(0:2^nposs-1)); %get all possible permu mask
-%permutes every possible indices:ConnPermu
-ind=repmat(find(DrivVecPos)',2^nposs,1);
-DrivPermuMask=DrivPermuMask.*ind;
-
-
-a=zeros(nnodes,1);
-%%%%%%%%%%%%%%%%THIS LINE IS DIFF  %%%%%%%%%%%%%%%%%%%%%
-DrivVecPermuPos=zeros(nnodes,1,2^nposs);
-DrivVecPermuPos(:,:,1)=a;
-for i=2:2^nposs
-    a=zeros(nnodes,1);
-    pos=DrivPermuMask(i,:);
-    a(pos(pos>0))=1;
-    DrivVecPermuPos(:,:,i)=a;
-end
-
-%define whole connection permutation matrix, 2^nposs in total
-%%%%%%%%%%%%%%%%%THIS LINE IS DIFF   %%%%%%%%%%%%%%%%%%%%
-DrivPermu=zeros(nnodes,1,2^nposs);
-for i=1:2^nposs
-    DrivPermu(:,:,i)=squeeze(DrivVecPermuPos(:,:,i))+DrivVecPermuDef;
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%END HERE%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+DrivPermu=permutation(DrivVec);
 
 
 
@@ -65,38 +34,11 @@ if isequal(ConnTemplate & ConnCompMat, ConnTemplate)==0
     disp(sprintf('The diagnol must all be set to 2'));
 end
 
-%implement permutation(get every possible permutation)
-%!!!!!!!etract must be's
-ConnMatPermuDef=(ConnMat==2);    
-
-%list all situation for could be's 
-ConnMatPos=(ConnMat==1);    %extract could be's
-nposs=nnz(ConnMatPos);  %# of 1s
-ConnPermuMask=str2vec(dec2bin(0:2^nposs-1)); %get all possible permu mask
-%permutes every possible indices:ConnPermu
-ind=repmat(find(ConnMatPos)',2^nposs,1);
-ConnPermuMask=ConnPermuMask.*ind;
-
-
-a=zeros(nnodes);
-ConnMatPermuPos=zeros(nnodes,nnodes,2^nposs);
-ConnMatPermuPos(:,:,1)=a;
-for i=2:2^nposs
-    a=zeros(nnodes);
-    pos=ConnPermuMask(i,:);
-    a(pos(pos>0))=1;
-    ConnMatPermuPos(:,:,i)=a;
-end
-
-%define whole connection permutation matrix, 2^nposs in total
-ConnPermu=zeros(nnodes,nnodes,2^nposs);
-for i=1:2^nposs
-    ConnPermu(:,:,i)=squeeze(ConnMatPermuPos(:,:,i))+ConnMatPermuDef;
-end
-
-
-
-
+ConnPermu=permutation (ConnMat);
+%t=cputime-t;
+%--------------------------------------------------------
+%----                   MODULATION                 ------
+%--------------------------------------------------------
 %-----------       define modulatory matrix    --------
-%Modu 
+%Modu
 
---
+++ permutation/permutation.m
@@ -0,0 +1,53 @@
+%This function requires an input matrix ('InputMat') which needs to be permuted
+%This input matrix can be any size, but with element values of 0/1/2 which
+%represent possibilities of each element.
+%0 means not allowed; 1 means could be; 2 means must be
+%For example, with a driving matrix for 5 nodes, the matrix can be like
+%follows (each element means can this driving feature affect the node or not):
+%                      | 2 |
+%                      | 1 |
+%                      | 0 |
+%                      | 0 |
+%                      | 1 |
+%This will be the input of the matrix
+%The output matrix will be a 5*1*4 matrix, which means there are 4
+%possibilities (two 1's, possibility # is 2^2), each can be pulled out from
+%PermuMat(:,:,i) (i means the ith possibility)
+
+function [PermuMat]=permutation (InputMat)
+    
+%implement permutation(get every possible permutation)
+%define # of rows and # of cols
+[nrow, ncol]=size(InputMat);
+
+%!!!!!!!etract must be's
+InputMatPermuDef=(InputMat==2);    
+
+%list all situation for could be's 
+InputMatPos=(InputMat==1);    %extract could be's
+nposs=nnz(InputMatPos);  %# of 1s
+InputPermuMask=str2vec(dec2bin(0:2^nposs-1)); %get all possible permu mask
+%permutes every possible indices:InputPermu
+ind=repmat(find(InputMatPos)',2^nposs,1);
+InputPermuMask=InputPermuMask.*ind;
+
+
+a=zeros(nrow,ncol);
+InputMatPermuPos=zeros(nrow,ncol,2^nposs);
+InputMatPermuPos(:,:,1)=a;
+for i=2:2^nposs
+    a=zeros(nrow,ncol);
+    pos=InputPermuMask(i,:);
+    a(pos(pos>0))=1;
+    InputMatPermuPos(:,:,i)=a;
+end
+
+
+%define whole Inputection permutation matrix, 2^nposs in total
+PermuMat=zeros(nrow,ncol,2^nposs);
+for i=1:2^nposs
+    PermuMat(:,:,i)=squeeze(InputMatPermuPos(:,:,i))+InputMatPermuDef;
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%END HERE%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
