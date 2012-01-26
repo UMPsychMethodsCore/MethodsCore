86eb9415b2ecead048f8405884036f87a06c6dae
Shijia Guo <guosj@umich.edu>
2012-01-26 15:08:44 -0500

    allD, all C, all M done, only need to combine to all DCM

--- permutation/permu.m
+++ permutation/permu.m
@@ -3,14 +3,14 @@
 %   permD - permissibility matrix of driving inputs
 %   permC - permissibility matrix of fixed connectivity
 %   permM - permissibility matrix of modulatory connectivity
-%   allD  - cell array of length a x 1 where a = # of possible driving
+%   allD  - cell array of length nD x 1 where nD = # of possible driving
 %   input configurations
-%   allC  - cell array of length b x 1 where b = # of poss connectivities
-%   allM  - cell array of length b x 1. Each cell will contain another cell
+%   allC  - cell array of length nC x 1 where nC = # of poss connectivities
+%   allM  - cell array of length nC x 1. Each cell will contain another cell
 %   array, size c* x 1 where c* is the number of possible modulatory
 %   connectivity configurations for a given fixed connectivity matrix
-%   (indexed by b)
-%   allDCM - cell array length 3 x (a * sum(c* over b))
+%   (indexed by nC)
+%   allDCM - cell array length 3 x (nD * sum(c* over nC))
 
 
 %%
@@ -23,37 +23,72 @@ nnodes=5;       %# of nodes
 %----                      DRIVING                 ------
 %--------------------------------------------------------
 %define driving vector
-DrivVec=[2,1,0,0,1]';
+permD=[2,1,0,0,1]';
 
 %---------    define driving permutation vector   -------
 %---------           defined as 'DrivPermu'         -----
 %implement permutation(get every possible permutation)
-DrivPermu=permutation(DrivVec);
-
+allD=permutation(permD);
+[nDrow, nDcol, nD]=size(allD);
 
 
 %--------------------------------------------------------
 %----                   CONNECTION                 ------
 %--------------------------------------------------------
 %connection matrix: 5*5 diagnal matrix w/ diagnol of 2
-ConnMat=2*diag(ones(nnodes,1));  
+permC=2*diag(ones(nnodes,1));  
 %user define
-ConnMat=[2,2,0,2,2;1,2,2,0,0;0,2,2,2,0;0,0,1,2,1;1,0,0,1,2];
+permC=[2,2,0,2,2;1,2,2,0,0;0,2,2,2,0;0,0,1,2,1;1,0,0,1,2];
 
-%---------    define connection permutation matrix -------
-%---------           defined as 'ConnPermu'         -----
+
 %determine whether diagnals are set to 2
-ConnCompMat=(ConnMat>1);
+ConnCompMat=(permC>1);
 ConnTemplate=diag(ones(nnodes,1));
 if isequal(ConnTemplate & ConnCompMat, ConnTemplate)==0
     disp(sprintf('The diagnol must all be set to 2'));
 end
 
-ConnPermu=permutation (ConnMat);
-%t=cputime-t;
+%---------    define connection permutation matrix -------
+%---------           defined as 'ConnPermu'         -----
+allC=permutation (permC);
+[nCrow,nCcol,nC]=size(allC);
+
+
 %--------------------------------------------------------
-%----                   MODULATION                 ------
+%----                   MODULATORY                 ------
 %--------------------------------------------------------
 %-----------       define modulatory matrix    --------
-%Modu
+permM=[0,2,0,1,0;1,0,0,0,0;0,1,0,0,0;0,0,2,0,1;0,0,0,1,0];
 
+%------define modulatory matrices for each connectivity matrices
+%------store all modu mats for the ith conn mat in permM{i}
+ModuCell=cell (1,nC);
+for i = 1 : nC
+    ModuClean=permM.*squeeze(allC(:,:,i));
+    allM{i}=permutation(ModuClean);
+end
+
+
+% %--------------------------------------------------------
+% %----                  COMBINE DCM                 ------
+% %--------------------------------------------------------
+% totalnM=0;
+% for i=1:nC
+%     [nMrow, nMcol, nM(i)]=size(allM{i});
+%     totalnM=totalnM+nM(i);
+% end
+%             
+% allDCM= cell(3,totalnM);
+% for i=1:nD
+%     for j=1:nC
+%         for k=1:nM(j)
+%             allDCM{1,sum(nM(1:j-1))+k}=allD(i);
+%             allDCM{2,sum(nM(1:j-1))+k}=allC(j);
+%             %
+%             M=cell(1,nM(j));
+%             allM{1,j}=M;
+%             allDCM{3,sum(nM(1:j-1))+k}=M{k};
+%         end
+%     end
+% end
+ t=cputime-t
