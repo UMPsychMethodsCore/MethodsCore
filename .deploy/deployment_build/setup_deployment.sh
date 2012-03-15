b419e08ccc590d2a5f9c769e08c3b68257863a0e
Daniel A Kessler <kesslerd@umich.edu>
2012-03-15 16:57:22 -0400

    more fixes

--- .deploy/deployment_build/setup_deployment.sh
+++ .deploy/deployment_build/setup_deployment.sh
@@ -5,7 +5,7 @@ thisdir=`pwd`
 #Make .local directory if it doesn't exist
 mkdir -p ${thisdir}/../../.local
 
-cd ${thisdir}/../.local
+cd ${thisdir}/../../.local
 touch deployment
 touch branch
 ssh-keygen -f deploy -N ""
