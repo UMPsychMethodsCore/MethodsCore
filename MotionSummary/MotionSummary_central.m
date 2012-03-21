ac93b46f0ce5c20aaa2518829190c54f2546586c
Joseph Heffernan <heffjos@umich.edu>
2012-03-21 13:14:45 -0400

    Made MotionSummary files compatible with mc_GenPath

--- MotionSummary/MotionSummary_central.m
+++ MotionSummary/MotionSummary_central.m
@@ -1,7 +1,11 @@
-addpath /net/dysthymia/slab/users/sripada/repos/methods_core/matlabScripts  %%%% this is for GeneratePath
+% addpath /net/dysthymia/slab/users/sripada/repos/methods_core/matlabScripts  %%%% this is for GeneratePath
 
 display ('-----')
-OutputPathFile = GeneratePath(OutputPathTemplate,Exp);
+%OutputPathFile = GeneratePath(OutputPathTemplate,Exp);
+OutputPathFile = mc_GenPath( sturct('Template',OutputPathTemplate,...
+                                    'suffix','.cxv',...
+                                    'mode','makeparentdir') );
+                                
 display('I am going to compute summary motion summary statistics');
 display(sprintf('The size of the lever arm I will use is %d',LeverArm));
 display(sprintf('The output will be stored here: %s', OutputPathFile));
@@ -9,24 +13,21 @@ display('These are the subjects:')
 display(SubjDir)
 display ('-----')
 
-
-
-
-
 clear CombinedOutput
 clear MotionPath
 for iSubject = 1:size(SubjDir,1)
 
     NumRun= size(SubjDir{iSubject,3},2);
+    
     for jRun = 1:NumRun
         Subject = SubjDir{iSubject,1};
         iRun=SubjDir{iSubject,3}(1,jRun);
         %       CharjRun = sprintf('%02d',jRun);
         Run = RunDir{iRun};
 
-        pathcallcmd=GeneratePathCommand(MotionPathTemplate);
-        MotionPath = eval(pathcallcmd);
-        
+        %pathcallcmd=GeneratePathCommand(MotionPathTemplate);
+        %MotionPath = eval(pathcallcmd);
+        MotionPath = mc_GenPath(MotionPathTemplate);
 
         
         MotionParameters = load (MotionPath);
@@ -39,12 +40,13 @@ end %%% loop over subjects
 
 %%%%%%% Save results to CSV file
 
-OutputPathFull=GeneratePath(OutputPathTemplate,Exp, OutputName);
-[OutputPath OutputName] = fileparts(OutputPathFull);
+%OutputPathFull=GeneratePath(OutputPathTemplate,Exp, OutputName);
+%[OutputPath OutputName] = fileparts(OutputPathFull);
 
-eval(sprintf('!mkdir -p %s',OutputPath));
-OutputPathFile=[OutputPath '/' OutputName];
-theFID = fopen([OutputPathFile,'.csv'],'w');
+%eval(sprintf('!mkdir -p %s',OutputPath));
+%OutputPathFile=[OutputPath '/' OutputName]; done in beginning
+
+theFID = fopen(OutputPathFile,'w');
 if theFID < 0
     fprintf(1,'Error opening the csv file!\n');
     return
@@ -56,17 +58,15 @@ for iSubject = 1:size(SubjDir,1)
     for jRun = 1:NumRun
         iRun=SubjDir{iSubject,3}(1,iRun);
         
-        
- 
  %%%%% Select appropriate output based on h user has set
  
- index=strfind(MotionPathTemplate,'Run');
- if size(index)>0
- RunString=RunDir{iRun};
- else
-     RunString=num2str(iRun);
+        index=strfind(MotionPathTemplate,'Run');
+        if size(index)>0
+            RunString=RunDir{iRun};
+        else
+            RunString=num2str(iRun);
 %    RunString = sprintf('%02d',jRun);
- end
+        end
  %%%%%%%%%%%%%%%%%%%%%%%%%%%
  
  
