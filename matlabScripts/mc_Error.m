4b013cc5c7eccc933850b7a9dab84b105d2f7dd7
Mike Angstadt <mangstad@med.umich.edu>
2012-03-26 18:12:33 -0400

    Created basic error structure of mc_Error.  Need to add logging features later.

---
+++ matlabScripts/mc_Error.m
@@ -0,0 +1,45 @@
+function mc_Error(errorString,varargin)
+% A utility function to make error handling and logging more compact
+% A single call to mc_Error will log the error to the global mc_LogName, 
+% bring up an error dialog box with your error message, and actually throw
+% an exception with the Matlab error function.
+%
+% FORMAT mc_Error(errorString,other variable arguments)
+%
+% errorString             This is a string message that will be displayed
+%                         to the user.  This can either be a simple string,
+%                         or it can take the format of an sprintf
+%                         formatting string using % format specifiers (see
+%                         sprintf documentation for more information about
+%                         these format strings).  
+%
+% other variables         If you are using the sprintf format string, you
+%                         need to pass in the other variables that need to
+%                         appear in the formatted string.
+%
+% EXAMPLES
+%    mc_Error('File does not exist.');
+%    mc_Error('File %s does not exist.',filename);
+%    mc_Error('File %s does not exist at path %s.',filename, folder);
+%    mc_Error('You specified %d images but I found %d.\nPlease double 
+%              check subject %s folder %s for images.',NumScan(iRun),
+%              size(P,1),subjDir,RunDir{iRun});
+
+    if (~isempty(varargin))
+        formatcount = size(strfind(errorString,'%'),2);
+        if (formatcount == size(varargin,2))
+            errormsg = sprintf(errorString,varargin{:});
+        else
+            mc_Error('Found %d format specifiers but %d other variables were provided.\n\nError String: %s\n',formatcount,size(varargin,2),errorString);
+            
+        end
+    else
+        errormsg = errorString;
+    end
+    
+    %add future code to call mc_Logger with loglevel Error
+    
+    errordlg(errormsg);
+    error(errormsg);
+    
+    
