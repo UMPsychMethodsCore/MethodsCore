4f6ee8b12e9297de75386550251b99d30a1610db
Daniel A Kessler <kesslerd@umich.edu>
2012-04-26 17:01:14 -0400

    added more cross validation demos to the pruning script

--- svmbatch/matlab_Scripts/pruning.m
+++ svmbatch/matlab_Scripts/pruning.m
@@ -1,9 +1,11 @@
 %% Simple feature pruning and testing
 
-ts=ttest(superflatmat(1:2:end,:),superflatmat(2:2:end,:),.25);
+ts=ttest(superflatmat(1:2:end,:),superflatmat(2:2:end,:),.01);
 ts(isnan(ts))=0;
 minimat=superflatmat(:,logical(ts));
 model=svmlearn(minimat,superlabel,' -c 0 -m 2000 -x 1 -o 100')
+
+%This works kick-ass. We get 100% LOOCV.
 
 %% Split halves
 
@@ -22,5 +24,49 @@ model_train=svmlearn(minimat_1,superlabel_1,'-o 100 -x 1')
 
 model_test=svmclassify(minimat_2,superlabel_2,model_train)
 
-% This does not work very well. It all hinges on which set the pruning
-% occurs.
+% This does not work quite as well. We get 100% LOOCV on the first half,
+% but only 60% on the second half.
+
+%% Paired SVM
+
+ts=ttest(superflatmat(1:2:end,:),superflatmat(2:2:end,:),.01);
+ts(isnan(ts))=0;
+
+superflatmat_pbompd=superflatmat(1:2:end,:)-superflatmat(2:2:end,:);
+superflatmat_mpdpbo=superflatmat(2:2:end,:)-superflatmat(1:2:end,:);
+
+superflatmat_paired=[superflatmat_pbompd;superflatmat_mpdpbo];
+
+superlabels_paired=[repmat(1,32,1);repmat(-1,32,1)];
+
+
+
+minimat_paired=superflatmat_paired(:,logical(ts));
+
+model=svmlearn(minimat_paired,superlabels_paired,' -c 0 -m 2000 -x 1 -o 10000')
+
+
+%% Paired Halves
+
+
+superflatmat_pbompd=superflatmat(1:2:end,:)-superflatmat(2:2:end,:);
+superflatmat_mpdpbo=superflatmat(2:2:end,:)-superflatmat(1:2:end,:);
+
+superflatmat_paired(1:2:64,:)=superflatmat_pbompd;
+superflatmat_paired(2:2:64,:)=superflatmat_mpdpbo;
+
+superflatmat_paired_1=superflatmat_paired(1:32,:);
+superflatmat_paired_2=superflatmat_paired(33:64,:);
+
+superlabels_paired_1=repmat([1; -1],16,1);
+superlabels_paired_2=repmat([1; -1],16,1);
+
+ts=ttest(superflatmat_paired_1(1:2:end,:),.01);
+ts(isnan(ts))=0;
+
+minimat_1=superflatmat_paired_1(:,logical(ts));
+minimat_2=superflatmat_paired_2(:,logical(ts));
+
+model_train=svmlearn(minimat_1,superlabels_paired_1,'-o 100 -x 1')
+
+model_test=svmclassify(minimat_2,superlabels_paired_2,model_train)
