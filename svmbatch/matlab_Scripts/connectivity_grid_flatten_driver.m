021ec538403b56e3fc0cf8b23d957a67167f7cf5
Daniel A Kessler <kesslerd@umich.edu>
2012-04-30 16:30:39 -0400

    Set stuff up for Rissman approach

--- svmbatch/matlab_Scripts/connectivity_grid_flatten_driver.m
+++ svmbatch/matlab_Scripts/connectivity_grid_flatten_driver.m
@@ -43,7 +43,8 @@ svmmode='mex' ;
 % ImageTemplate = '[Exp]/Subjects/[Subject]/TASK/func/[Run]/'
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-ConnTemplate = '[Exp]/FirstLevel/[Subject]/12mmGrid_19/12mmGrid_19_corr.mat';
+ConnTemplate = '[Exp]/FirstLevel/[Subject]/MSIT/HRF/FixDur/TBTGrid/TBTGrid_corr.mat';
+% ConnTemplate = '[Exp]/FirstLevel/[Subject]/12mmGrid_19/12mmGrid_19_corr.mat';
 
 OutputTemplate = '[Exp]/SVM/Connectivity/12mmGrid_19/Train.dat';
 
@@ -64,10 +65,10 @@ SubjDir = {
 
 '5001/Tx1',+1;
 '5001/Tx2',-1;
-'5002/Tx1',+1;
-'5002/Tx2',-1;
-'5003/Tx1',+1;
-'5003/Tx2',-1;
+% '5002/Tx1',+1;
+% '5002/Tx2',-1;
+% '5003/Tx1',+1;
+% '5003/Tx2',-1;
 '5004/Tx1',+1;
 '5004/Tx2',-1;
 '5005/Tx1',+1;
@@ -176,6 +177,7 @@ for iSub=1:size(SubjDir,1)
     conPath=mc_GenPath(conPathTemplate);
     conmat=load(conPath);
     rmat=conmat.rMatrix;
+%     sprintf('Subject %s has variance %s',Subject,var(rmat(~isnan(rmat))))
     switch svmmode
         case 'classic'
             connectivity_grid_flatten(rmat,outPath,Example, cleanconMat,1)
--- svmbatch/matlab_Scripts/pruning.m
+++ svmbatch/matlab_Scripts/pruning.m
@@ -52,16 +52,16 @@ model=svmlearn(minimat_paired,superlabels_paired,' -c 0 -m 2000 -x 1 -o 10000')
 superflatmat_pbompd=superflatmat(1:2:end,:)-superflatmat(2:2:end,:);
 superflatmat_mpdpbo=superflatmat(2:2:end,:)-superflatmat(1:2:end,:);
 
-superflatmat_paired(1:2:64,:)=superflatmat_pbompd;
-superflatmat_paired(2:2:64,:)=superflatmat_mpdpbo;
+superflatmat_paired(1:2:60,:)=superflatmat_pbompd;
+superflatmat_paired(2:2:60,:)=superflatmat_mpdpbo;
 
-superflatmat_paired_1=superflatmat_paired(1:32,:);
-superflatmat_paired_2=superflatmat_paired(33:64,:);
+superflatmat_paired_1=superflatmat_paired(1:30,:);
+superflatmat_paired_2=superflatmat_paired(31:60,:);
 
-superlabels_paired_1=repmat([1; -1],16,1);
-superlabels_paired_2=repmat([1; -1],16,1);
+superlabels_paired_1=repmat([1; -1],15,1);
+superlabels_paired_2=repmat([1; -1],15,1);
 
-ts=ttest(superflatmat_paired_1(1:2:end,:),.01);
+[ts tp]=ttest(superflatmat_paired_1(1:2:end,:),0,.001);
 ts(isnan(ts))=0;
 
 minimat_1=superflatmat_paired_1(:,logical(ts));
@@ -76,19 +76,27 @@ model_test=svmclassify(minimat_2,superlabels_paired_2,model_train)
 superflatmat_pbompd=superflatmat(1:2:end,:)-superflatmat(2:2:end,:);
 superflatmat_mpdpbo=superflatmat(2:2:end,:)-superflatmat(1:2:end,:);
 
-superflatmat_paired(1:2:64,:)=superflatmat_pbompd;
-superflatmat_paired(2:2:64,:)=superflatmat_mpdpbo;
+superflatmat_paired(1:2:60,:)=superflatmat_pbompd;
+superflatmat_paired(2:2:60,:)=superflatmat_mpdpbo;
 
-superflatmat_paired_1=superflatmat_paired(1:32,:);
-superflatmat_paired_2=superflatmat_paired(33:64,:);
+superflatmat_paired_1=superflatmat_paired(1:30,:);
+superflatmat_paired_2=superflatmat_paired(31:60,:);
 
-superlabels_paired_1=repmat([1; -1],16,1);
-superlabels_paired_2=repmat([1; -1],16,1);
+superlabels_paired_1=repmat([1; -1],15,1);
+superlabels_paired_2=repmat([1; -1],15,1);
 
-superlabels_paired_1=randsample(superlabels_paired_1,32)
-superlabels_paired_2=randsample(superlabels_paired_2,32)
+switchers_1=sort(randsample(1:15,8));
+switchers_2=sort(randsample(1:15,8));
 
-ts=ttest(superflatmat_paired_1(1:2:end,:),.01);
+superlabels_paired_1(switchers_1*2-1)=-1;
+superlabels_paired_1(switchers_1*2)=+1;
+
+superlabels_paired_2(switchers_2*2-1)=-1;
+superlabels_paired_2(switchers_2*2)=+1;
+
+
+
+[ts tp]=ttest(superflatmat_paired_1(switchers_1*2,:),0,.001);
 ts(isnan(ts))=0;
 
 minimat_1=superflatmat_paired_1(:,logical(ts));
@@ -97,3 +105,47 @@ minimat_2=superflatmat_paired_2(:,logical(ts));
 model_train=svmlearn(minimat_1,superlabels_paired_1,'-o 100 -x 1')
 
 model_test=svmclassify(minimat_2,superlabels_paired_2,model_train)
+
+%% Iterative LOOCV with iterative pruning?
+
+superflatmat_pbompd=superflatmat(1:2:end,:)-superflatmat(2:2:end,:);
+superflatmat_mpdpbo=superflatmat(2:2:end,:)-superflatmat(1:2:end,:);
+
+superflatmat_paired(1:2:60,:)=superflatmat_pbompd;
+superflatmat_paired(2:2:60,:)=superflatmat_mpdpbo;
+
+pruneLOO=zeros(30,size(superflatmat,2));
+
+models_train={};
+models_test={};
+
+for iL=1:30
+    subjects=[1:(iL-1) (iL+1):30];
+    indices=sort([subjects*2 subjects*2-1]);
+    
+    
+    
+    train=superflatmat_paired(indices,:);
+    trainlabels=repmat([1; -1],size(train,1)/2,1);
+    
+    prune=ttest(train(1:2:end,:),0,.0001);
+    prune(isnan(prune))=0;
+    pruneLOO(iL,:)=prune;
+    
+    train=train(:,logical(prune));
+    test=superflatmat_paired([iL*2-1 iL*2],logical(prune));
+    
+    models_train{iL}=svmlearn(train,trainlabels,'-o 100 -x 0');
+    
+    models_test{iL}=svmclassify(test,[1 ; -1],model_train);
+
+end
+
+pruneIntersect=sum(pruneLOO,1);
+pruneIntersect(pruneIntersect~=30)=0;
+pruneIntersect(pruneIntersect==30)=1;
+
+supertrain = superflatmat_paired(:,logical(pruneIntersect));
+superlabels=repmat([1; -1],30,1);
+
+supermodel = svmlearn(supertrain,superlabel,'-o 100 -x 1')
