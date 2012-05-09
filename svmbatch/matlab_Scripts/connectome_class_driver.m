a1af479394af58ac58107ebafd17ad710e691e99
Daniel A Kessler <kesslerd@umich.edu>
2012-05-09 12:37:31 -0400

    rewrote central part of script to do fractional thresholding for pruning

--- svmbatch/matlab_Scripts/connectome_class_driver.m
+++ svmbatch/matlab_Scripts/connectome_class_driver.m
@@ -11,7 +11,12 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Exp = '/net/data4/MAS/';
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Paired mode. Set this to 1 if your data is organized in sequential
+%%% pairs
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+pairedSVM=1;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %  Path where your images are located
@@ -42,6 +47,11 @@ RunDir = {
 
 };
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% How many features of each LOOCV iteration should be retained after
+%%% pruning?
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+nFeatPrune = 100;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% The list of subjects to process
 %%% The format is 'subjectfolder',subject number in masterfile,[runs to include]
@@ -140,18 +150,25 @@ outPath=mc_GenPath(outPathTemplate);
 
 %% Loop 1 to figure out valid connection points
 
-cleanconMat=ones(size(rmat));
+nSubs=size(SubjDir,1);
 
 for iSub=1:size(SubjDir,1)
+
     Subject = SubjDir{iSub,1};
     conPath=mc_GenPath(conPathTemplate);
     conmat=load(conPath);
     rmat=conmat.rMatrix;
-    
+    if iSub==1
+        cleanconMat=ones(size(rmat));
+        nfeat=size(rmat(:));
+    end
     cleanconMat(isnan(rmat) | isinf(rmat) | rmat==0) = 0; %For all indices in rmat that are NaN, zero out cleanconMat
 end
 
 %% Loop 2 to write out flattened results
+
+
+superlabel=zeros(nSubs,1);
 
 for iSub=1:size(SubjDir,1)
     Subject = SubjDir{iSub,1};
@@ -159,10 +176,79 @@ for iSub=1:size(SubjDir,1)
     conPath=mc_GenPath(conPathTemplate);
     conmat=load(conPath);
     rmat=conmat.rMatrix;
-%     sprintf('Subject %s has variance %s',Subject,var(rmat(~isnan(rmat))))
+    %     sprintf('Subject %s has variance %s',Subject,var(rmat(~isnan(rmat))))
+    if iSub==1
+        superflatmat=zeros(nSubs,size(connectivity_grid_flatten(rmat,cleanconMat),2));
+    end
+    superflatmat(iSub,:)=connectivity_grid_flatten(rmat, cleanconMat);
+    superlabel(iSub,1)=Example;
 
-            superflatmat(iSub,:)=connectivity_grid_flatten(rmat, cleanconMat);
-            superlabel(iSub,1)=Example;
-    
 end
 
+%% Organize your paired data
+
+if pairedSVM==1
+
+    superflatmat_p1=superflatmat(1:2:end,:)-superflatmat(2:2:end,:);
+    superflatmat_p2=superflatmat(2:2:end,:)-superflatmat(1:2:end,:);
+
+    superflatmat_paired(1:2:size(superflatmat,1),:) = superflatmat_p1;
+    superflatmat_paired(2:2:size(superflatmat,1),:) = superflatmat_p2;
+
+    pruneLOO=zeros(size(superflatmat,1)/2,size(superflatmat,2));
+
+    models_train={};
+    models_test=[];
+    
+    for iL=1:(size(superflatmat,1)/2)
+        fprintf(1,'\nCurrently running LOOCV on subject %.0f of %.0f.\n',iL,size(superflatmat,1)/2)
+        
+        subjects=[1:(iL-1) (iL+1):30];
+        indices=sort([subjects*2 subjects*2-1]);
+
+
+
+        train=superflatmat_paired(indices,:);
+        trainlabels=repmat([1; -1],size(train,1)/2,1);
+
+        % Identify the fraction of features that are greater than zero, or
+        % less than zero (whichever is larger). This indicates a consistent
+        % signed direction. Do it just for one pair, since the second pair
+        % is the first * -1
+        
+        fractions=max(  [sum(train(1:2:end,:)>0,1)/size(train(1:2:end,:),1) ;sum(train(1:2:end,:)<0,1)/size(train(1:2:end,:),1)]  );
+        
+        [d pruneID] = sort(fractions);
+        pruneID=pruneID((end-(nFeatPrune-1)):end);
+        
+%         prune=ttest(train(1:2:end,:),0,.00001);
+        %% Add tau-b support here
+%         prune(isnan(prune))=0;
+%         pruneLOO(iL,:)=prune;
+
+        train=train(:,pruneID);
+        test=superflatmat_paired([iL*2-1 iL*2],pruneID);
+
+        models_train{iL}=svmlearn(train,trainlabels,'-o 100 -x 0');
+
+        models_test(iL)=svmclassify(test,[1 ; -1],models_train{iL});
+
+
+    end
+     fprintf(1,'\nLOOCV Done\n\n');
+end
+
+%% Report performance
+
+fprintf(1,'\nLOOCV performance is %.0f out of %.0f, for %.0f%% accuracy.\n\n',...
+    size(models_test,2)-sum(models_test),...
+    size(models_test,2),...
+    100*(size(models_test,2)-sum(models_test))/size(models_test,2));
+
+%% Identification of Consensus Connectome
+
+
+pruneIntersect=all(pruneLOO,1);
+
+
+    
