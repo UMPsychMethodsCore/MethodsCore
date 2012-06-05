186734451cc517deb035aba114a7ad11db7dc97c
Shijia Guo <guosj@umich.edu>
2012-06-05 10:28:55 -0400

    central script unflatten function replaced

--- svmbatch/matlab_Scripts/connectome_class_driver_mc_central.m
+++ svmbatch/matlab_Scripts/connectome_class_driver_mc_central.m
@@ -419,14 +419,19 @@ if (exist('Vizi') &&  Vizi==1)
 
     %% Reconstruct Consensus Discrim Power into Edges File
 
-    % Identify linear indices of elements that survive flattening
-    connectomeIDx=zeros(nROI);
-    connectomeIDx(:)=1:(nROI^2);
-    connectomeIDx_flat = connectivity_grid_flatten(connectomeIDx,ones(nROI));
+%     % Identify linear indices of elements that survive flattening
+%     connectomeIDx=zeros(nROI);
+%     connectomeIDx(:)=1:(nROI^2);
+%     connectomeIDx_flat = connectivity_grid_flatten(connectomeIDx,ones(nROI));
+% 
+%     % Build square matrix, and use linear indices above to insert discrim power
+%     LOOCV_discrimpower_consensus_square = zeros(nROI);
+%     LOOCV_discrimpower_consensus_square(connectomeIDx_flat) = LOOCV_discrimpower_consensus;
 
-    % Build square matrix, and use linear indices above to insert discrim power
-    LOOCV_discrimpower_consensus_square = zeros(nROI);
-    LOOCV_discrimpower_consensus_square(connectomeIDx_flat) = LOOCV_discrimpower_consensus;
+
+% replacing above code with function "unflatten"
+LOOCV_discrimpower_consensus_square = unflatten (LOOCV_discrimpower_consensus, nROI);
+
     LOOCV_discrimpower_consensus_square_binarized = LOOCV_discrimpower_consensus_square;
     LOOCV_discrimpower_consensus_square_binarized(LOOCV_discrimpower_consensus_square_binarized~=0) = 1 ;
 
