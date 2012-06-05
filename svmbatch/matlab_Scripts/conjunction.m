3ce3a472bd936c8eb39112186e14b22eacf77204
Shijia Guo <guosj@umich.edu>
2012-06-05 10:29:13 -0400

    conjunction function v1.0

--- svmbatch/matlab_Scripts/conjunction.m
+++ svmbatch/matlab_Scripts/conjunction.m
@@ -1,3 +1,3 @@
-ROIA = load /net/dysthymia/slab/users/guosj/repos/MethodsCore/svmbatch/matlab_Scripts/FirstLevel/5001/Tx1/MSIT/HRF/FixDur/TBTGrid/TBTGrid_parameters.mat;
-ROIB = load /net/dysthymia/slab/users/guosj/repos/MethodsCore/svmbatch/matlab_Scripts/FirstLevel/5001/Tx2/MSIT/HRF/FixDur/TBTGrid/TBTGrid_parameters.mat;
-
+function [connectome, ROIcoordinates] = conjunction()
+%% find common ROIs
+[commonROI, coA, coB] = intersect(ROIA, ROIB, 'rows');
