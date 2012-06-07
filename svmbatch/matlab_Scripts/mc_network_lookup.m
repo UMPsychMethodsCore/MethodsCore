77674306b7181fa53c637a8e96d94935421f8759
Daniel A Kessler <kesslerd@umich.edu>
2012-06-07 14:55:00 -0400

    improved help for mc_network_lookup

--- svmbatch/matlab_Scripts/mc_network_lookup.m
+++ svmbatch/matlab_Scripts/mc_network_lookup.m
@@ -1,6 +1,13 @@
 function [ labeled_voxels ] = mc_network_lookup( mapFile, mmlist )
-%UNTITLED1 Summary of this function goes here
-%   Detailed explanation goes here
+%mc_network_lookup  A function that, given a list of MNI coords, will
+%retrieve the value at corresponding voxel from an image
+%   Input Args
+%       mapFile     -   A path to a hdr file that will be used for network
+%                       lookup
+%       mmlist      -   A nROI*3 matrix of MNI coords
+%   Output Args
+%       labeled_voxels  -   An nROI*4 matrix, same as input, but with
+%                           looked up values tacked on in 4th column
 
 labeled_voxels = zeros(size(mmlist,1),4);
 labeled_voxels(:,1:3) = mmlist;
