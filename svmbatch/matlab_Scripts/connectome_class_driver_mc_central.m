f8e50aba920c72d0f53743057dd15da1c530e36d
Daniel A Kessler <kesslerd@umich.edu>
2012-08-06 09:01:50 -0400

    fixed bug in calculation of weighted_nuisance regressors in paired cases

--- svmbatch/matlab_Scripts/connectome_class_driver_mc_central.m
+++ svmbatch/matlab_Scripts/connectome_class_driver_mc_central.m
@@ -327,7 +327,9 @@ if strcmpi(svmtype,'paired')
         
         % correct for nuisance regressors if they exist
         if exist('DoNuisanceCorrection','var') && DoNuisanceCorrection==1
-            weighted_nuisance=NuisanceRegressors(:,:,iCond) * curContrast(iCond); % weight by contrasting info
+            for iCond = 1:condNum
+                weighted_nuisance=NuisanceRegressors(:,:,iCond) * curContrast(iCond); % weight by contrasting info
+            end
             weighted_nuisance=weighted_nuisance(logical(contrastAvail(:,iContrast)),:,:); %prune based on contrast availability
             nuisance = sum(weighted_nuisance,3); % calculate delta or whatever contrast wants
             
