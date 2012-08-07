8d23e55df7f9f4c6c5e18f858af505bb42777635
Daniel A Kessler <kesslerd@umich.edu>
2012-08-07 10:33:20 -0400

    mc_CovariateCorrection now mean centers the design matrix. It also returns values for intercepts, and returns as its first argument the corrected value, which is the intercepts + residuals. Updated documentation to clarify this

--- matlabScripts/mc_CovariateCorrection.m
+++ matlabScripts/mc_CovariateCorrection.m
@@ -1,4 +1,4 @@
-function [ residuals, betas ] = mc_CovariateCorrection( Y, X )
+function [ corrected, residuals, betas, intercepts ] = mc_CovariateCorrection( Y, X )
 %MC_COVARIATECORRECTION Correction for a series of covariates using
 %multiple regression
 % 
@@ -9,10 +9,15 @@ function [ residuals, betas ] = mc_CovariateCorrection( Y, X )
 % This program will assume the same design matrix for all of your features,
 % thus enabling much more rapid computation of the residuals.
 % 
+% We will also mean center all of your covariates so that your intercepts
+% are interpretable as the original means.
+% 
 % NOTE  -   This script will prepend a column of ones to your X matrix to
 % model an intercept, so you do not need to include it.
 
 
+% Mean center all your covariates
+X = mc_SweepMean(X);
 
 % Prepend a constant to the predictor matrix
 X = horzcat(ones(size(X,1),1),X);
@@ -21,7 +26,9 @@ betas =   pinv(X)*Y;
 
 residuals = Y - X*betas;
 
+intercepts = X(:,1)*betas(1,:);
 
+corrected = intercepts + residuals;
 
  
 
