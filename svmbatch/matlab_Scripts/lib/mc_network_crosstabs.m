97c4696658aacd9326cdc8f8a005f2273f480c2d
Daniel A Kessler <kesslerd@umich.edu>
2012-09-10 14:17:04 -0400

    started work on network crosstabs function

---
+++ svmbatch/matlab_Scripts/lib/mc_network_crosstabs.m
@@ -0,0 +1,12 @@
+function [ crosstabs, networks ] = mc_network_crosstabs ( edge_bin, path_param, path_networkmap )
+%MC_NETWORK_CROSSTABS Crosstabulate your edge support by networks
+% 
+% 
+
+%% Figure out networks
+
+mc_
+
+
+end
+
