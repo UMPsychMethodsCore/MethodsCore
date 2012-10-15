2547afbd6c4f10366b8adf8465f1523ef043986e
Joseph Heffernan <heffjos@umich.edu>
2012-10-15 12:55:36 -0400

    Removed all file checking since it is performed in qc_checkopt.m.  Also changed how the wall of shame text file is written so that it can be parsed easier.

--- QualityChecks/CheckMetrics/qc_mc_central.m
+++ QualityChecks/CheckMetrics/qc_mc_central.m
@@ -1,85 +1,52 @@
 function Results = qc_mc_central(Opt)
-% Opt.
-%   Exp
-%   Postpend.
-%     Subjects  {Subjects,IncludedRuns}
-%     Runs
-%   Lists.
-%     Subjects
-%     Runs
-%   File
-%     Func
-%   Detected
-%   Thresh
-nsubjects = size(Opt.List.Subjects,1);
-ImageExp  = ['^' Opt.File.Func '.*nii'];
+%
+% Input: 
+%   Opt.
+%       Exp  -  Experiment top dir
+%       List.
+%           Subjects {Subjects,IncludedRuns}
+%           Runs     - list of run name folders
+%       Postpend.
+%           Exp      - what goes after Exp
+%           Subjects - what goes after .List.Subjects{:,1}
+%           Runs     - what goes after Runs
+%       FileExp      - Regexp for file names
+%       OutlierText  - full path to output text file
+%       Thresh       - z-score threshold value
+%
+Results = -1;
 
 % Check everything first
-Exp = Opt.Exp;
-for i = 1:nsubjects
-    Subject = Opt.List.Subjects{i,1};
-    for k = Opt.List.Subjects{i,2}
-        Run      = Opt.List.Runs{k};
-        ImageDir.Template = Opt.ImageDir;
-        ImageDir.mode = 'Check';
-        funcDir = mc_GenPath(ImageDir);
-            
-        funcFile = spm_select('FPList',funcDir,ImageExp);
-        if isempty(funcFile)
-            fprintf(1,'FATAL ERROR: No function file in directory: %s\n',funcDir);
-            fprintf(1,' * * * A B O R T I N G * * *\n');
-            return;
-        end
-        
-        if size(funcFile,1) > 1
-            fprintf(1,'FATAL ERROR: Expected only one 4D functional image in directory: %s\n',funcDir);
-            fprintf(1,'Please check Opt.File.Func filter\n');
-            fprintf(1,' * * * A B O R T I N G * * *\n');
-            return;
-        end
-    end
-end
-
-fid = fopen(Opt.Detected,'w');
-if fid == -1
-    fprintf(1,'Cannot write to %s\n',Opt.Detected);
-    fprintf(1,' * * * A B O R T I N G * * *\n');
+checkedFiles = qc_checkopt(Opt);
+if isempty(checkedFiles)
     return;
 end
 
-% Perform calculations
-fprintf(1,'Calculating diagnostics...\n');
-for i = 1:nsubjects
-    Subject = Opt.List.Subjects{i,1};
-    for k = Opt.List.Subjects{i,2}
-        Run      = Opt.List.Runs{k};
-        funcDir  = mc_GenPath(Opt.ImageDir);
-        funcFile = spm_select('FPList',funcDir,ImageExp);
+fid = fopen(Opt.OutlierText,'w');
+fprintf(fid,'WALL OF SHAME\n');
 
-        fprintf(1,'Subject: %s Run: %s\n',Subject,Run);
-        QC_metrics = qc_metrics(funcFile);
-        if ~isempty(QC_metrics)
-            save('results.mat','QC_metrics');
-            % save in subject directory later
+% Perform calculations
+fprintf(1,'Calculating metrics...\n');
+for i = 1:size(checkedFiles,1)
+    fprintf(1,'Subject: %s Run: %s\n',checkedFiles{i,2},checkedFiles{i,3});
+    metrics = qc_metrics(checkedFiles{i,1});
+    if ~isempty(metrics)
+        [pathstr file ext] = fileparts(checkedFiles{i,1});
+        save(fullfile(pathstr,'qc_metrics.mat'),'metrics');
         
-            %options.format     = 'pdf';
-            %options.outputDir  = funcDir;
-            %options.showCode   = false;
-            %options.catchError = false;
-            %publish('qc_publish.m',options);
-            qc_report(QC_metrics,fullfile(funcDir,'qc_report'));
-            close('all');
-            
-            [z t] = find(QC_metrics.SliceZScore > Opt.Thresh);
-            if ~isempty(z)
-                fprintf(fid,'%s\n',QC_metrics.Fname);
-                for l = 1:length(z)
-                    fprintf(fid,'slice: %d timepoint: %d z-score: %f mse: %f\n',z(l),t(l),QC_metrics.SliceZScore(z(l),t(l)),QC_metrics.SliceTmse(z(l),t(l)));
-                end
+        [z t] = find(metrics.SliceZScore > Opt.Thresh);
+        if ~isempty(z)
+            fprintf(fid,'Image:\n');
+            fprintf(fid,'%s\n',metrics.Fname);
+            fprintf(fid,'{\n');
+            for l = 1:length(z)
+                fprintf(fid,'\tslice: %3d timepoint: %3d z-score: %2.3f mse: %3.3f\n',z(l),t(l),metrics.SliceZScore(z(l),t(l)),metrics.SliceTmse(z(l),t(l)));
             end
+            fprintf(fid,'}\n');
         end
     end
 end
+        
 fclose('all');
 fprintf(1,'All done!\n');
 Results = 1;
