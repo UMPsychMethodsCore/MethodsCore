a81d67f393aeb6ba375e50d62781ea04d1a3b235
Joseph Heffernan <heffjos@umich.edu>
2012-10-16 17:46:00 -0400

    Added variable descriptions in qc_slice_mc_template.m.

--- QualityChecks/CheckSlice/qc_slice_mc_template.m
+++ QualityChecks/CheckSlice/qc_slice_mc_template.m
@@ -1,11 +1,26 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% If you have any questions read the pdf documentation or contact
+%%% MethodsCore at methodscore@umich.edu
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear;
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% The folder that contains your subject folders
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Opt.Exp  = '/zubdata/oracle7/Researchers/heffjos/TestSubject';
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% The list of subjects to process
+%%% The format is {'subjectfolder',[runs to include]}
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Opt.List.Subjects = {
                      'CM2001NTX',[1 2 3 4 5 6 7];
                      'CM2002NTX',[1 2 3 4 5 6 7];
                     };
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% A list of run folders where the script can find the images to use
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Opt.List.Runs = {
                   'run_01';
                   'run_02';
@@ -16,20 +31,42 @@ Opt.List.Runs = {
                   'run_07';
                 };
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% What to put after Opt.Exp
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Opt.Postpend.Exp = '';
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% What to put after Opt.List.Subjects{:,i}
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Opt.Postpend.Subjects = 'day4/func';
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% What to put after Opt.List.Runs{i}
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Opt.Postpend.Runs = '';
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Prefix of scan images to use
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Opt.FileExp = 'run';
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Text file to output detected slice outliers
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Opt.OutlierText = '/zubdata/oracle7/Researchers/heffjos/TestSubject/run_detected.txt';
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Z score threshold; slices with absolute z scores greater than this 
+%%% value will be considered as outliers
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Opt.Thresh = 4;
 
+global mcRoot;
 mcRoot = '/zubdata/oracle7/Researchers/heffjos/MethodsCore';
 
 addpath(fullfile(mcRoot,'matlabScripts'));
-addpath(fullfile(mcRoot,'QualityChecks/CheckMetrics'));
+addpath(fullfile(mcRoot,'QualityChecks','CheckSlice'));
 addpath(fullfile(mcRoot,'SPM','SPM8','spm8_with_R4667'));
 
 qc_slice_mc_central(Opt);
