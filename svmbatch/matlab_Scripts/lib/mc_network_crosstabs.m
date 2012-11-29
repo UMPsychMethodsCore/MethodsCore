b8a451b67e913aeaeda33873e044adb36de5fe93
Daniel A Kessler <kesslerd@umich.edu>
2012-11-29 16:52:42 -0500

    mc_network_crosstabs can now accept an optional argument that will do a NearestNetworkNode lookup on the nodes of interest prior to reporting the crosstabs. This should result in fewer unlabeled edges in the cross tabulations.

--- svmbatch/matlab_Scripts/lib/mc_network_crosstabs.m
+++ svmbatch/matlab_Scripts/lib/mc_network_crosstabs.m
@@ -1,4 +1,4 @@
-function [ table ] = mc_network_crosstabs ( edge_bin, path_param, path_networkmap )
+function [ table ] = mc_network_crosstabs ( edge_bin, path_param, path_networkmap, NetNborRad )
 %MC_NETWORK_CROSSTABS Crosstabulate your edge support by networks
 % 
 %   INPUT
@@ -11,6 +11,14 @@ function [ table ] = mc_network_crosstabs ( edge_bin, path_param, path_networkma
 % 
 %       path_networkmap -   A fully qualified path to your network map HDR.
 %                           Used to look up values for network mapping.
+
+%       NetNborRad      -   OPTIONAL | If specified, the function will attempt to identify
+%                           any nodes that would have carried a label of zero otherwise.
+%                           It does this with a call to mc_NearestNetworkNode. This argument
+%                           specifies the 'radius' argument for mc_NearestNetworkNode, see
+%                           that function's help for full details.
+%                           WARNING - mc_NearestNetworkNode is not very fast, so if you have
+%                           a large consensus this will add a LOT of time. 
 % 
 % NOTE - Because some network maps include 0 as a region, the first
 % row/column are dedicated to network 0.
@@ -24,6 +32,24 @@ nets=mc_connectome_get_edge_attribute(path_param,4,path_networkmap);
 maxNet=max(nets(:));
 
 nets=nets(:,logical(edge_bin)); % subset to only include those requested
+
+%% Lookup Nearest Network Neighbor
+if exist('NetNborRad','var')
+    xs = mc_connectome_get_edge_attribute(path_param,1,path_networkmap);
+    ys = mc_connectome_get_edge_attribute(path_param,2,path_networkmap);
+    zs = mc_connectome_get_edge_attribute(path_param,3,path_networkmap);
+    
+    node1 = [xs(1,:)' ys(1,:)' zs(1,:)'];
+    node2 = [xs(2,:)' ys(2,:)' zs(2,:)'];
+    
+    node1 = node1(logical(edge_bin),:);
+    node2 = node2(logical(edge_bin),:);
+    
+    node1net = mc_NearestNetworkNode(node1,NetNborRad);
+    node2net = mc_NearestNetworkNode(node2,NetNborRad);
+    
+    nets = [node1net ; node2net;];
+end
 
 
 %% Built up your table
