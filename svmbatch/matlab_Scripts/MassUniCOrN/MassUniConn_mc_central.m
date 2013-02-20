6816795545f8284dd16bff3c85814ca20df2df44
Yu Fang <yfang@umich.edu>
2013-02-20 13:07:29 -0500

    Add Mass Univariate Connectomme analysis template and central script

---
+++ svmbatch/matlab_Scripts/MassUniCOrN/MassUniConn_mc_central.m
@@ -0,0 +1,127 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                      %
+%                   MassUnivariate Connectomme Analysis                % 
+%                           Central Script                             %
+%                                                                      %
+%                                                                      %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%% Load the design matrix data
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+DesMtrxPathCheck  = struct('Template',DesMtrxTemplate,'mode','check');
+DesMtrxPath  = mc_GenPath(DesMtrxPath);
+s  = load(DesMtrxPath);
+s.subs(:,2) = num2cell(1); % add a second column
+s.design(:,2:end) = mc_SweepMean(s.design(:,2:end)); % mean center covariates of the first column
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%               
+%%%%% Load subjects data
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+CorrPathCheck = struct('Template',CorrTemplate,'mode','check');
+CorrPath = mc_GenPath(CorrPathCheck);
+data = mc_load_connectomes_unpaired(s.subs,CorrPath);  % input: subject list
+data = mc_connectome_clean(data);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%% Network
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ParamPathCheck = struct('Template',ParamTemplate,'mode','check');
+ParamPath = mc_GenPath(ParamPathCheck);
+param = load(ParamPath);
+
+roiMNI = param.parameters.rois.mni.coordinates;
+nets = mc_NearestNetworkNode(roiMNI,5);
+sq_blanks = zeros(size(roiMNI,1));
+
+for iNet = 0:max(nets)
+    for jNet = iNet:max(nets)
+        csq_blanks = sq_blanks;
+        csq_blanks(nets==iNet,nets==jNet) = 1;  % csq: current square
+        csq_blanks(nets==jNet,nets==iNet) = 1;
+        csq_blanks=triu(csq_blanks,1);
+        csq = mc_flatten_upper_triangle(csq_blanks);
+        netmask{iNet+1,jNet+1} = logical(csq); % network index starts from zero
+    end    
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%% Fit the real model
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[~, ~, ~, ~, t, p] = mc_CovariateCorrection(data,s.design,1,2); % 2: tell the code which column we care about 
+
+ts = sign(t(2,:)); % figure out the sign
+
+ts(ts==+1) = 3; % map the positive values to 3 (flip the direction since autism is 1)
+ts(ts==-1) = 2; % map the negative values to 2 (flip direction)
+ts(ts==+0) = 1;
+
+a.pruneColor.values = ts;
+a.prune = p(2,:) < thresh(1);
+a.NetworkLabels = nets;
+a.DotDilateMat = [1 0; -1 0; 0 1; 0 -1; % cross
+                   -1 1; 1 1; -1 -1; 1 -1; %fill out square
+                   -2 0; 0 2; 2 0; 0 -2]; % cross around square
+
+a.pruneColor.map = [1 1 1; % make 1 white
+                    1 0 0; % make 2 red
+                    0 0 1; % make 3 blue
+                    ];
+a.Shading.Enable = enable;
+a.Shading.StatMode = statmode;
+a.Shading.ePDF = squeeze(perms(:,:,1,1,:));
+
+celltot =   %%% Need refactorize mc_Takgraph_Lowlevel first;     % Count Edges Per Cell
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% Permutations
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% for perms.count
+%First two dimensions will index the network structure. 
+%Third dimension will index different threshold values. 
+%Fourth dimension will index repetitions of the permutation test
+
+% for perms.mean
+% First two dimensions will index nework structure
+% Third dimension will index repetitions of permutation test
+
+perms = zeros(nNet,nNet,1,numel(thresh),nRep); %5D object, permutation, cppi needs 5th dimension
+pos = zeros(nNet,nNet,1,numel(thresh),nRep); %5D object, count how many positive
+neg = zeros(nNet,nNet,1,numel(thresh),nRep); %5D object, count how many negative
+
+% Quick timing test   %%% need this or not?
+tic
+mc_uni_permute(data,netmask,thresh,permcol,s.design);  % do a permutation
+toc
+
+for i=1:nRep
+    [perms(:,:,:,:,i) pos(:,:,:,:,i) neg(:,:,:,:,i)] = mc_uni_permute(data,netmask,thresh,permcol,s.design,1);
+    fprintf(1,'%g\n',i)
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% Calc Cell-Level Statistics
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% Call TakGraph_lowlevel
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% Network Contingency Analyses
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+
+
+              
