0c7eef1a1b0782e7050beb6c7d3e29d19adaf296
Joseph Heffernan <heffjos@umich.edu>
2013-02-25 16:53:12 -0500

    MotionSummary_mc_central.m now calls mc_FD_calculation directly in the script.  The central script also writes out a csv for each subject in the same directory as the realign parameters file.  Usually this file is created in the run directory for a subject.  The csv holds the FDjudge variable calculate from mc_FD_calculation.

--- QualityChecks/CheckMotion/MotionSummary_mc_central.m
+++ QualityChecks/CheckMotion/MotionSummary_mc_central.m
@@ -13,8 +13,7 @@ fprintf(1, '-----\n');
 clear CombinedOutput
 clear MotionPath
 for iSubject = 1:size(SubjDir,1)
-    Subject = SubjDir{iSubject,1}; 
-    
+    Subject = SubjDir{iSubject,1};     
     for jRun = 1:size(SubjDir{iSubject,3},2)
         RunNum = SubjDir{iSubject,3}(jRun);
         Run    = RunDir{RunNum};
@@ -25,48 +24,46 @@ for iSubject = 1:size(SubjDir,1)
         
         [pathstr,name,ext] = fileparts(MotionPath);
         if any(strcmp(ext,{'.par','.dat'}))
-            Output = euclideanDisplacement(MotionParameters,LeverArm,FDLeverArm,FDcriteria);
+            Output = euclideanDisplacement(MotionParameters,LeverArm);
+            [FD FDjudge] = mc_FD_calculation(MotionParameters, FDcriteria, FDLeverArm, ScansBefore, ScansAfter);
         else
-            Output = euclideanDisplacement(fliplr(MotionParameters),LeverArm,FDLeverArm,FDcriteria);
+            Output = euclideanDisplacement(fliplr(MotionParameters),LeverArm);
+            [FD FDjudge] = mc_FD_calculation(fliplr(MotionParameters), FDcriteria, FDLeverArm, ScansBefore, ScansAfter);
         end
+        
+        Output.meanFD       = mean(FD);
+        Output.censorvector = FDjudge;
+        Output.nonzeroFD    = nnz(FDjudge);
+        
         if ~isstruct(Output) && Output == -1; return; end;
         CombinedOutput{iSubject,jRun} = Output;
         
-        OutputCensorVectorFile = mc_GenPath(struct('Template',OutputCensorVector,...
-            'suffix','.mat',...
-            'mode','makeparentdir'));
-        cv = Output.censorvector;
-        save(OutputCensorVectorFile,'cv');
-        
+        if ~isempty(FDjudge)
+            [pathstr file ext] = fileparts(MotionPath);
+            fdCsv = fullfile(pathstr, 'fdOutliers.csv');
+            fdFid = fopen(fdCsv, 'w');
+            if fdFid == -1
+                fprintf(1, 'Cannot write fdOutliers.csv for subject %s\n', Subject);
+                fprintf(1, 'Check permissions for path: %s\n', pathstr);
+            else
+                ind = find(FDjudge(:) > 0);
+                [m n] = ind2sub(size(FDjudge), ind);
+                for i = 1:(length(m) - 1)
+                    fprintf(fdFid, 'scan%d,', m(i));
+                end
+                fprintf(fdFid, 'scan%d\n', m(end));
+                
+                for i = 1:size(FDjudge, 1)
+                    for k = 1:(size(FDjudge, 2) - 1)
+                        fprintf(fdFid, '%d,', FDjudge(i, k));
+                    end
+                    fprintf(fdFid, '%d\n', FDjudge(i, end));
+                end
+                fclose(fdFid);
+            end
+        end                
     end
 end
-
-%%%%% For the datasets that have different sessions %%%%%%
-% for iSubject = 1:size(SubjDir,1)
-%     Subject = SubjDir{iSubject,1};
-%     
-%     for jSession = 1:size(SessionDir,1)
-%         SessionNum = SubjDir{iSubject,2}(jSession);
-%         Session    = SessionDir{SessionNum};
-%         for kRun = 1:size(SubjDir{iSubject,3},2)
-%             RunNum = SubjDir{iSubject,3}(kRun);
-%             Run    = RunDir{RunNum};
-%             
-%             MotionPathCheck  = struct('Template',MotionPathTemplate,'mode','check');
-%             MotionPath       = mc_GenPath(MotionPathCheck);
-%             MotionParameters = load(MotionPath);
-%             
-%             [pathstr,name,ext] = fileparts(MotionPath);
-%             if any(strcmp(ext,{'.par','.dat'}))
-%                 Output = euclideanDisplacement(MotionParameters,LeverArm,FDLeverArm,FDcriteria);
-%             else
-%                 Output = euclideanDisplacement(fliplr(MotionParameters),LeverArm,FDLeverArm,FDcriteria);
-%             end
-%             if ~isstruct(Output) && Output == -1; return; end;
-%             CombinedOutput{iSubject,jSession,kRun} = Output;
-%         end
-%     end
-% end
 
 %%%%%%% Save results to CSV file
 theFID = fopen(OutputPathFile,'w');
@@ -75,99 +72,31 @@ if theFID < 0
     return;
 end
 
-%%%%%%%%% Output Values for each Run of each Session of each Subject%%%%%%%%%%%%%%%%
-% fprintf(theFID,'Subject,Session,Run,maxSpace,meanSpace,maxAngle,meanAngle,meanFD,nonzeroFD\n'); %header
-% for iSubject = 1:size(SubjDir,1)
-%     Subject = SubjDir{iSubject,1};
-%     for jSession = 1:size(SessionDir,1)
-%         SessionNum = SubjDir{iSubject,2}(jSession);
-%         Session    = SessionDir{SessionNum};
-%         for kRun = 1:size(SubjDir{iSubject,3},2)
-%             RunNum = SubjDir{iSubject,3}(kRun);
-%             
-%             %%%%% Select appropriate output based on h user has set
-%             index=strfind(MotionPathTemplate,'Run');
-%             if size(index)>0
-%                 RunString=RunDir{kRun};
-%             else
-%                 RunString=num2str(kRun);
-%             end
-%             %%%%%%%%%%%%%%%%%%%%%%%%%%%
-%             
-%             fprintf(theFID,'%s,%s,%s,',Subject,Session,RunString);
-%             fprintf(theFID,'%.4f,',CombinedOutput{iSubject,jSession,kRun}.maxSpace);
-%             fprintf(theFID,'%.4f,',CombinedOutput{iSubject,jSession,kRun}.meanSpace);
-%             fprintf(theFID,'%.4f,',CombinedOutput{iSubject,jSession,kRun}.maxAngle);
-%             fprintf(theFID,'%.4f,',CombinedOutput{iSubject,jSession,kRun}.meanAngle);
-%             fprintf(theFID,'%.4f,',CombinedOutput{iSubject,jSession,kRun}.meanFD);
-%             fprintf(theFID,'%.4f\n',CombinedOutput{iSubject,jSession,kRun}.nonzeroFD);
-%         end
-%     end
-% end
-
-
-% %%%%%%%%% Output Values for each Run of each Subject%%%%%%%%%%%%%%%%
-% fprintf(theFID,'Subject,Run,maxSpace,meanSpace,maxAngle,meanAngle,meanFD,nonzeroFD\n'); %header
-% for iSubject = 1:size(SubjDir,1)
-%     Subject = SubjDir{iSubject,1};
-%     for jRun = 1:size(SubjDir{iSubject,3},2)
-%         RunNum = SubjDir{iSubject,3}(jRun);
-%         
-%         %%%%% Select appropriate output based on h user has set
-%         index=strfind(MotionPathTemplate,'Run');
-%         if size(index)>0
-%             RunString=RunDir{jRun};
-%         else
-%             RunString=num2str(jRun);
-%         end
-%         %%%%%%%%%%%%%%%%%%%%%%%%%%%
-%         
-%         fprintf(theFID,'%s,%s,',Subject,RunString);
-%         fprintf(theFID,'%.4f,',CombinedOutput{iSubject,jRun}.maxSpace);
-%         fprintf(theFID,'%.4f,',CombinedOutput{iSubject,jRun}.meanSpace);
-%         fprintf(theFID,'%.4f,',CombinedOutput{iSubject,jRun}.maxAngle);
-%         fprintf(theFID,'%.4f,',CombinedOutput{iSubject,jRun}.meanAngle);
-%         fprintf(theFID,'%.4f,',CombinedOutput{iSubject,jRun}.meanFD);
-%         fprintf(theFID,'%.4f\n',CombinedOutput{iSubject,jRun}.nonzeroFD);
-%     end
-% end
-
-%%%%%%%%%%%%%%%% Output averaged values along runs for each Subject %%%%%
-fprintf(theFID,'Subject,maxSpace,meanSpace,maxAngle,meanAngle,meanFD,nonzeroFD\n'); %header
-
+fprintf(theFID,'Subject,Run,maxSpace,meanSpace,sumSpace,maxAngle,meanAngle,sumAngle,meanFD,nonzeroFD\n'); %header
 for iSubject = 1:size(SubjDir,1)
     Subject = SubjDir{iSubject,1};
-%     for jRun = 1:size(SubjDir{iSubject,3},2)
-%         RunNum = SubjDir{iSubject,3}(jRun);
+    for jRun = 1:size(SubjDir{iSubject,3},2)
+        RunNum = SubjDir{iSubject,3}(jRun);
         
-%         %%%%% Select appropriate output based on h user has set
-%         index=strfind(MotionPathTemplate,'Run');
-%         if size(index)>0
-%             RunString=RunDir{jRun};
-%         else
-%             RunString=num2str(jRun);
-%         end
+        %%%%% Select appropriate output based on h user has set
+        index=strfind(MotionPathTemplate,'Run');
+        if size(index)>0
+            RunString=RunDir{jRun};
+        else
+            RunString=num2str(jRun);
+        end
         %%%%%%%%%%%%%%%%%%%%%%%%%%%
-        temp = [CombinedOutput{iSubject,:}];
         
-        % Average over runs
-        FinalOutput.maxSpace = mean([temp(:).maxSpace]);
-        FinalOutput.meanSpace = mean([temp(:).meanSpace]);
-        FinalOutput.maxAngle = mean([temp(:).maxAngle]);
-        FinalOutput.meanAngle = mean([temp(:).meanAngle]);         
-        FinalOutput.meanFD = mean([temp(:).meanFD]);
-        
-        % the total excluded points over runs 
-        FinalOutput.nonzeroFD = sum([temp(:).nonzeroFD]);
-        
-        fprintf(theFID,'%s,%s,',Subject);
-        fprintf(theFID,'%.4f,',FinalOutput.maxSpace);
-        fprintf(theFID,'%.4f,',FinalOutput.meanSpace);
-        fprintf(theFID,'%.4f,',FinalOutput.maxAngle);
-        fprintf(theFID,'%.4f,',FinalOutput.meanAngle);
-        fprintf(theFID,'%.4f,',FinalOutput.meanFD);
-        fprintf(theFID,'%.4f\n',FinalOutput.nonzeroFD);
-%     end
+        fprintf(theFID,'%s,%s,',Subject,RunString);
+        fprintf(theFID,'%.4f,',CombinedOutput{iSubject,jRun}.maxSpace);
+        fprintf(theFID,'%.4f,',CombinedOutput{iSubject,jRun}.meanSpace);
+        fprintf(theFID,'%.4f,', CombinedOutput{iSubject, jRun}.sumSpace);
+        fprintf(theFID,'%.4f,',CombinedOutput{iSubject,jRun}.maxAngle);
+        fprintf(theFID,'%.4f,',CombinedOutput{iSubject,jRun}.meanAngle);
+        fprintf(theFID,'%.4f,', CombinedOutput{iSubject, jRun}.sumAngle);
+        fprintf(theFID,'%.4f,',CombinedOutput{iSubject,jRun}.meanFD);
+        fprintf(theFID,'%.4f\n',CombinedOutput{iSubject,jRun}.nonzeroFD);
+    end
 end
 
 fclose(theFID);
