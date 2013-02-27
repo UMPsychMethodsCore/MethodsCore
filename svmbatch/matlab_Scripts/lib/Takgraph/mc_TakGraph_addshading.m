20d1e416ac2ffa90e1e978bb731c813509804335
Daniel A Kessler <kesslerd@umich.edu>
2013-02-27 16:40:59 -0500

    big rewrite of add transparency function

--- svmbatch/matlab_Scripts/lib/Takgraph/mc_TakGraph_addshading.m
+++ svmbatch/matlab_Scripts/lib/Takgraph/mc_TakGraph_addshading.m
@@ -1,26 +1,48 @@
-function mc_TakGraph_addshading( a )
+function [ a ] =  mc_TakGraph_addshading( a )
 %MC_TAKGRAPH_ADDSHADING 
 % Based on the result of stats analysis, add shading over the TakGraph at
 % the stats significant area
 %       INPUTS
-%                       a.stats.cellsign        -       A nNet x nNet matrix: whether a given cell was selected as having more edges "on" than expected by chance. Coding is...
-%                                                               1 - Not significant
-%                                                               2 - Positive signicant
-%                                                               3 - Negative significant
-%                                                               4 - Undirectional Significant 
+%                       a.shading.color         -       nNet x nNet x 3 matrix of colors to shade. Third dimension is an RGB triple.
+%                                                       Alternatively, a RGB row vector to to be used as color for all cells    
 %                       a.mediator.sorted       -       1 x nROI matrix of sorted network labels, which will help with finding the start and end point of each cell.
-%                       a.shading.transparency  -       Transparency of the shading blocks
-%
+%                       a.stats.FDR.hypo        -       We will only shade cells where this value is 1    
+%               OPTIONAL
+%                       a.shading.transparency  -       nNet x nNet matrix of opacity values for shading. 0 is transparent, 1 opaque.
+%                                                       Alternatively, provide a scalar, and all cells will have identical shading.
+%                                                       Defaults to .3    
+%                       a.shading.shademask     -       Allows you to override behavior of a.stats.FDR.hypo. nNet x nNet logical
+%                                                       matrix of which cells to shade    
 
+
+set(0,'currentfigure',a.h)
 hold on;
 
-stats_result = a.stats.cellsign;
-transp       = a.shading.transparency;
+if isfield(a.shading,'shademask') % if a shademask is given, use that
+    shademask = a.shading.shademask;
+else
+    shademask = a.stats.FDR.hypo == 1;
+end
+
+if numel(size(a.shading.color)) < 3;
+    shadecolor(1,1,:) = a.shading.color;
+    shadecolor = repmat(shadecolor,[size(shademask), 1]);
+end
+
+% figure out transparency setting
+if ~isfield(a.shading,'transparency') % if unset, default
+    transp = .3;
+elseif numel(a.shading.transparency) == 1 % if just one, replicate it
+    transp = repmat(a.shading.transparency,size(shademask));
+else % if a matrix, use as is
+    transp = a.shading.transparency;
+end
+    
 sorted       = a.mediator.sorted;
 
 % if only one transparency is given, replicate it for use everywhere
 if numel(transp)==1     
-    transp=repmat(transp,size(stats_result));
+    transp=repmat(transp,size(shademask));
 end
 
 sorted_new = sorted';
@@ -30,50 +52,19 @@ stops=[find(jumps) - 1; size(sorted_new,1)];
 starts = starts - 0.5;
 stops = stops + 0.5;
 
-for i = 1:size(stats_result,1)
-    for j = i:size(stats_result,2)
-        if (i == j)  % half shading on the diagonal cells
-            switch stats_result(i,j)
-            case 1
-                continue
-            case 2  
+for i = 1:size(shademask,1)
+    for j = i:size(shademask,2)
+        if shademask(i,j)
+            if (i == j)  % half shading on the diagonal cells
                 shade_x = [starts(j),stops(j),stops(j)];
                 shade_y = [starts(i),starts(i),stops(i)];                          
-                fill(shade_x,shade_y,'r','FaceAlpha',transp(i,j));
-            case 3 
-                shade_x = [starts(j),stops(j),stops(j)];
-                shade_y = [starts(i),starts(i),stops(i)]; 
-                fill(shade_x,shade_y,'b','FaceAlpha',transp(i,j));
-            case 4
-                shade_x = [starts(j),stops(j),stops(j)];
-                shade_y = [starts(i),starts(i),stops(i)]; 
-                fill(shade_x,shade_y,'y','FaceAlpha',transp(i,j));
-            otherwise 
-                warning('Unexpected value in the results, please check!')
-                continue
-            end
-        else
-            switch stats_result(i,j)
-            case 1
-                continue
-            case 2  
+                fill(shade_x,shade_y,squeeze(shadecolor(i,j,:))','FaceAlpha',transp(i,j));
+            else
                 shade_x = [starts(j),stops(j),stops(j),starts(j)];
                 shade_y = [starts(i),starts(i),stops(i),stops(i)];                          
-                fill(shade_x,shade_y,'r','FaceAlpha',transp(i,j));
-            case 3 
-                shade_x = [starts(j),stops(j),stops(j),starts(j)];
-                shade_y = [starts(i),starts(i),stops(i),stops(i)];
-                fill(shade_x,shade_y,'b','FaceAlpha',transp(i,j));
-            case 4
-                shade_x = [starts(j),stops(j),stops(j),starts(j)];
-                shade_y = [starts(i),starts(i),stops(i),stops(i)];
-                fill(shade_x,shade_y,'y','FaceAlpha',transp(i,j));
-            otherwise 
-                warning('Unexpected value in the results, please check!')
-                continue
+                fill(shade_x,shade_y,squeeze(shadecolor(i,j,:))','FaceAlpha',transp(i,j));
             end
         end          
-        
     end
 end
 
