cf348dd4280646d09c3b34e3e66f10b19aeaabaf
Daniel A Kessler <kesslerd@umich.edu>
2013-02-27 11:38:10 -0500

    cleaned up and simplified network mediator

--- svmbatch/matlab_Scripts/lib/Takgraph/mc_Network_mediator.m
+++ svmbatch/matlab_Scripts/lib/Takgraph/mc_Network_mediator.m
@@ -1,56 +1,42 @@
 function [ a ] = mc_Network_mediator( a )
-% MC_NETWORK_MEDIATOR
-% 
-% This function will implement the cell counting based on network distributions. This is usually used as the first step
-% of TakGraph generation. 
+% mc_Network_FeatRestruct - restructure your vector of features into a square matrix, 
+% and permute the square matrix to follow network structure
 % 
 %       INPUTS
 % 
 %               REQUIRED
-%                       a.prune                 -       1 x nFeat logical matrix of features to plot
-%                       a.NetworkLabels         -       1 x nROI matrix of network labels. This will be used literally.
+%                       a.values                -       1 x nFeat matrix of feature values
+%                                                       For downstream functions to work, only allowed values are...
+%                                                               1 - Edge Not Significant
+%                                                               2 - Edge Significant & Positive
+%                                                               3 - Edge Significant & Negative    
+%                       a.NetworkLabels         -       1 x nROI matrix of network labels. This will be used as is (no network expansion)
 %
 %               OPTIONAL
-%                       a.pruneColor.values     -       1 x nFeat matrix of color values that will index into a.pruneColor.map
-%                       a.pruneColor.map        -       A colormap object that will be directly indexed by pruneColor.values. 
+%                       a.pruneColor.map        -       A colormap object that will be directly indexed by a.values. Not
+%                                                       required for this function, but useful downstream.
 %              
 %       OUTPUTS(New subfields of a)
 % 
-%               a.mediator                      -       A set of variables that are useful for the following functions, and these variables contain:
-%                       a.mediator.square       -       Transform a.pruneColor.values from a 1 x nFeat matrix to a sorted upper triangular matrix. 
-%                       a.mediator.square_prune -       Transform a.prune from a 1 x nFeat matrix to a sorted upper triangular matrix.      
+%               a.mediator                      -       A set of variables that are useful for the many downstream functions, containing
+%                       a.mediator.square       -       a.values restructured as network-sorted upper triangular matrix (nROI x nROI)
 %                       a.mediator.sorted       -       1 x nROI matrix of sorted network labels.
 % 
 
-% Deal with coloration, if enabled
-if(isfield(a,'pruneColor'))
-    a.pruneColor.values(~logical(a.prune)) = 1; % Set colors outside of prune to 1, so they will use first colormap color
-else % If no a.pruneColor passed, set it up as if the colormap goes white, black, and the values are 1s and 2s
-    a.pruneColor.values                    = zeros(size(a.prune));
-    a.pruneColor.values(logical(a.prune))  = 2;
-    a.pruneColor.values(~logical(a.prune)) = 1;
-    a.pruneColor.map                       = [1 1 1; 0 0 0]; % Define a colormap where 1 = white, 2 = black
-end
-
 % Make your square matrix
-square       = mc_unflatten_upper_triangle(a.pruneColor.values);
-square_prune = mc_unflatten_upper_triangle(a.prune);
+square       = mc_unflatten_upper_triangle(a.values);
 
 % Sort the square by networks
 [sorted, sortIDX] = sort(a.NetworkLabels);
 
 square       = square(sortIDX,sortIDX);
-square_prune = square_prune(sortIDX,sortIDX);
 
 % Get it all back on the upper triangle
 square       = triu(square + square',1); 
-square_prune = triu(square_prune + square_prune');
 
 % Save the result back to a
 a.mediator.square       = square;
-a.mediator.square_prune = square_prune;
 a.mediator.sorted       = sorted;
-
 
 end
 
