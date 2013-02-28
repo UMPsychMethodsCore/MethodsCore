069cf569670efdfc5ab344c072ed71e5df17fceb
Daniel A Kessler <kesslerd@umich.edu>
2013-02-28 10:40:53 -0500

    fixed some syntax error, and made sure that divide by zero is properly handled as inf

--- svmbatch/matlab_Scripts/lib/Takgraph/mc_TakGraph_CalcShadeColor.m
+++ svmbatch/matlab_Scripts/lib/Takgraph/mc_TakGraph_CalcShadeColor.m
@@ -10,13 +10,13 @@ function [ a ] = mc_TakGraph_CalcShadeColor ( a )
 %                                               dimension you have RGB triples. If cell's sig edges are all positive,
 %                                               it will be [1 0 0], if all negative, [0 0 1], if 50/50, [.5 0 .5], etc
 
-a.shading.color=zeros([size(a.cellcount.celltot],3)); % initialize colors
+a.shading.color=zeros([size(a.cellcount.celltot),3]); % initialize colors
 
 cellpropPos = a.cellcount.cellpos ./ a.cellcount.celltot;
-cellpropNeg = 1 - cellproppos;
+cellpropNeg = 1 - cellpropPos;
 
-cellpropPos(isnan(cellpropPos)) = 0; % zero out any NaN that might come from divide by 0
-cellpropNeg(isnan(cellpropNeg)) = 0; % zero out any NaN that might come from divide by 0
+cellpropPos(isinf(cellpropPos)) = 0; % zero out any Inf that might come from divide by 0
+cellpropNeg(isinf(cellpropNeg)) = 0; % zero out any Inf that might come from divide by 0
 
 a.shading.color(:,:,1) = cellpropPos;
 a.shading.color(:,:,3) = cellpropNeg;
