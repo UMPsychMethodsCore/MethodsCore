876c6a082593a55345098273664a022d270f85ca
Daniel A Kessler <kesslerd@umich.edu>
2013-03-07 14:09:38 -0500

    made indexing of netmask more robust so that it can work with arbitrary network structure, rather than requiring a monotonic series

--- svmbatch/matlab_Scripts/MassUniCOrN/MassUniConn_mc_central.m
+++ svmbatch/matlab_Scripts/MassUniCOrN/MassUniConn_mc_central.m
@@ -61,31 +61,34 @@ sq_blanks = zeros(size(roiMNI,1));
 
 %%% Build Netmask
 % need to generalize this to support cPPI and resting
+netSort = sort(unique(nets)); % identify the unique networks
+
+nNet   =  numel(netSort); % how many unique nets do you have?
+
 
 
 switch matrixtype
     case 'upper'
 
-      for iNet = 0:max(nets)
-          for jNet = iNet:max(nets)
+      for iNet = 1:nNet
+          for jNet = iNet:nNet
               csq_blanks = sq_blanks;
               csq_blanks(nets==iNet,nets==jNet) = 1;  % csq: current square
               csq_blanks(nets==jNet,nets==iNet) = 1;
-              csq_blanks=triu(csq_blanks,1);
               csq = mc_flatten_upper_triangle(csq_blanks);
-              netmask{iNet+1,jNet+1} = logical(csq); % network index starts from zero
-          end    
+              netmask{iNet,jNet} = logical(csq); 
+          end
       end
   case 'nodiag'
 % for yoke 1 & 2
-    for iNet = 0:max(nets)
-        for jNet = iNet:max(nets)
+    for iNet = 1:nNet
+        for jNet = iNet:nNet
             csq_blanks = sq_blanks;
             csq_blanks(nets==iNet,nets==jNet) = 1;
             csq_blanks(nets==jNet,nets==iNet) = 1;
             csq_blanks = csq_blanks - diag(diag(csq_blanks)); % zero out the diagonal
             csq = reshape(csq_blanks,size(roiMNI,1)^2,1);
-            netmask{iNet+1,jNet+1} = logical(csq);
+            netmask{iNet,jNet} = logical(csq);
         end    
     end
 end
