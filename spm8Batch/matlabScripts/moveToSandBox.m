18de091b1873d6067b5e68b9770236693d3362e0
Mike Angstadt <mangstad@sopho.psych.med.umich.edu>
2013-03-08 16:51:44 -0500

    fixed moveToSandbox merge problem

--- spm8Batch/matlabScripts/moveToSandBox.m
+++ spm8Batch/matlabScripts/moveToSandBox.m
@@ -1,12 +1,27 @@
+% - - - - - - - - - - - - - - - - - - - - 
 %
-% function [CS SandBoxPID Images2Write] = moveToSandBox(sourceDir,sourceVolume,SandBoxPID)
+% Robert C Welsh
+% Ann Arbor
 %
+% Copyright 2011-2012
+%
+% Adding ability to pass the file extension for the source.
+%
+% function [CS SandBoxPID Images2Write] = moveToSandBox(sourceDir,sourceVolume,SandBoxPID,sourceExtension)
+%
+% - - - - - - - - - - - - - - - - - - - - 
 
-function [CS SandBoxPID Images2Write] = moveToSandBox(sourceDir,sourceVolume,SandBoxPID)
+function [CS SandBoxPID Images2Write] = moveToSandBox(sourceDir,sourceVolume,SandBoxPID,sourceExtension)
+
+% Check to see if sourceExtension was specified
+
+if exist('sourceExtension') == 0
+  sourceExtension = '.*.nii';
+end
 
 % Default is to use what we have on the source directory
 
-Images2Write = spm_select('ExtFPList',sourceDir,['^' sourceVolume '.*.nii'],inf);
+Images2Write = spm_select('ExtFPList',sourceDir,['^' sourceVolume sourceExtension],inf);
 CS           = 0;
 
 % Does the sandbox even exist?
@@ -17,29 +32,36 @@ CS           = 0;
 % impact of other code? 
 
 if exist(SandBoxPID) == 7
-  FILESTOMOVE=dir([sourceDir '/' sourceVolume '*.nii']);
-  %
-  % Big assumption is one nifti file per directory
-  %
-  tic;
-  % This can only handle one file at a time, and WILL break with multiple - RCWelsh 2012-04-07
-  if length(FILESTOMOVE) > 1
-    fprintf('Move than one nifti file in source directory %s, sandbox method can only handle a single.\n',sourceDir);
+  if length(findstr('img',sourceExtension)) > 0
+    fprintf('Only nifti files are supported for use with the sandbox\n');
     fprintf('Turning off sandbox\n');
     CS=0
     SandBoxPID='';
-  else
-    fprintf('Moving %s to sandbox:%s',FILESTOMOVE(1).name,SandBoxPID);
-    [CS CM CMID] = copyfile(fullfile(sourceDir,FILESTOMOVE(1).name),SandBoxPID);
-    xtoc=toc;
-    fprintf('; It took %f seconds\n',xtoc);
-    if CS == 1
-      Images2Write = spm_select('ExtFPList',SandBoxPID,['^' sourceVolume '.*.nii'],inf);
-      % Log in the process log that we are using the sand box.
-      UMBatchLogProcess(sourceDir,sprintf('Using sandbox : %s to work on file %s',SandBoxPID,FILESTOMOVE(1).name));
-    else
-      fprintf('Failed to move to sandbox, turning off sandbox\n');      
+  else    
+    FILESTOMOVE=dir([sourceDir '/' sourceVolume sourceExtension(2:end)]);
+    %
+    % Big assumption is one nifti file per directory
+    %
+    tic;
+    % This can only handle one file at a time, and WILL break with multiple - RCWelsh 2012-04-07
+    if length(FILESTOMOVE) > 1
+      fprintf('More than one nifti file in source directory %s, sandbox method can only handle a single.\n',sourceDir);
+      fprintf('Turning off sandbox\n');
+      CS=0
       SandBoxPID='';
+    else
+      fprintf('Moving %s to sandbox:%s',FILESTOMOVE(1).name,SandBoxPID);
+      [CS CM CMID] = copyfile(fullfile(sourceDir,FILESTOMOVE(1).name),SandBoxPID);
+      xtoc=toc;
+      fprintf('; It took %f seconds\n',xtoc);
+      if CS == 1
+	Images2Write = spm_select('ExtFPList',SandBoxPID,['^' sourceVolume sourceExtension],inf);
+	% Log in the process log that we are using the sand box.
+	UMBatchLogProcess(sourceDir,sprintf('Using sandbox : %s to work on file %s',SandBoxPID,FILESTOMOVE(1).name));
+      else
+	fprintf('Failed to move to sandbox, turning off sandbox\n');      
+	SandBoxPID='';
+      end
     end
   end
 else
