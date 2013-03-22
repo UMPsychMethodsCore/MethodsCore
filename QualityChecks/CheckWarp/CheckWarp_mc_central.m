12c4bd1c8119033da06998cd66446b574d8c5c13
Joseph Heffernan <heffjos@umich.edu>
2013-03-21 23:18:11 -0400

    Changed CheckWarp to display only the first run and the template image per subject.

--- QualityChecks/CheckWarp/CheckWarp_mc_central.m
+++ QualityChecks/CheckWarp/CheckWarp_mc_central.m
@@ -16,41 +16,33 @@ for iSubject = 1:size(SubjDir,1)
 
     Subject=SubjDir{iSubject};
 
-    fprintf('\n\n\nPerforming check registration for subject: %s\n\n\n', Subject);
+    fprintf('\n\n\nPerforming check registration for subject: %s', Subject);
     
-    for jRun = 1:length(SubjDir{iSubject,3})
+    Run = RunDir{SubjDir{iSubject,3}(1)};
     
-        Run=RunDir{jRun};
-
-        fprintf('\n\n\nPerforming check registration for run: %s\n\n\n', Run);
-
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        ImagePathCheck = struct('Template',ImageTemplate,...
-                                'mode','check');
-        ImagePath = mc_GenPath(ImagePathCheck);
-
-        fileName = fullfile( ImagePath, strcat(FilePrefix,'*.nii') );
-        ImagePathFile=dir(fileName);
-        ImagePathName=ImagePathFile(1).name;
-
-        WarpTemplateCheck = struct('Template',WarpTemplate,...
-                                   'mode','check');
-        WarpTemplate = mc_GenPath(WarpTemplateCheck);
-        data = {
-                [WarpTemplate];
-                fullfile(ImagePath, [ImagePathName ',1']);
-                fullfile(ImagePath, [ImagePathName ',2']);
-                fullfile(ImagePath, [ImagePathName ',3']);
-               };
-
-        CheckRegJob.jobs{1}.util{1}.checkreg.data=data;
-
-        spm_jobman('run',CheckRegJob.jobs);
-
-        fprintf(1, 'Press any key to continue\n');
-        pause;
+    fprintf('Using run: %s\n\n\n', Run);
+    
+    ImagePathCheck = struct('Template', ImageTemplate, 'mode', 'check');
+    ImagePath = mc_GenPath(ImagePathCheck);
+    
+    spmFilt = ['^' FilePrefix '.*nii'];
+    displayImage = spm_select('ExtFPList', ImagePath, spmFilt, 1);
+    if isempty(displayImage) || size(displayImage, 1) > 2
+        fprintf(1, 'Invaild file prefix for subject %s\n', Subject);
+        fprintf(1, ' * * * S K I P P I N G * * *\n');
+        continue;
     end
- 
+    
+    WarpTemplateCheck = struct('Template', WarpTemplate, 'mopde', 'check');
+    WarpTemplate = mc_GenPath(WarpTemplateCheck);
+    data = {WarpTemplate displayImage};
+    
+    CheckRegJob.jobs{1}.util{1}.checkreg.data = data;
+    
+    spm_jobman('run', CheckRegJob.jobs);
+    
+    fprintf(1, 'Press any key to continue\n');
+    pause;
 end % Loop over subjects
 close all
 fprintf(1, 'All done!\n');   
