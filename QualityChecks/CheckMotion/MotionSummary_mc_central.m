11148ddd4340ea7c6e81834b7f289b55ffca6ca3
Joseph Heffernan <heffjos@umich.edu>
2013-04-05 12:04:33 -0400

    Motion summary writes the correct run now in the summary csv.

--- QualityChecks/CheckMotion/MotionSummary_mc_central.m
+++ QualityChecks/CheckMotion/MotionSummary_mc_central.m
@@ -82,7 +82,7 @@ for iSubject = 1:size(SubjDir,1)
         %%%%% Select appropriate output based on h user has set
         index=strfind(MotionPathTemplate,'Run');
         if size(index)>0
-            RunString=RunDir{jRun};
+            RunString=RunDir{RunNum};
         else
             RunString=num2str(jRun);
         end
