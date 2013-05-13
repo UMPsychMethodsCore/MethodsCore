eaae8f9c8576deb4ebd083fbe4a0cd39048f3f5d
Robert C. Welsh <rcwelsh@med.umich.edu>
2013-05-13 19:51:22 -0400

    Ready for testing?

--- ConnTool/ConnTool_batch_mc_central.m
+++ ConnTool/ConnTool_batch_mc_central.m
@@ -20,30 +20,30 @@ if (strcmp(spmver,'SPM8')==1)
 end
 
 if ~isempty(GreyFile)
-  GreyMatterTemplate  = [AnatomyMaskPath '/' GreyFile]
+    GreyMatterTemplate  = [AnatomyMaskPath '/' GreyFile]
 end
 
 if ~isempty(WhiteFile)
-  WhiteMatterTemplate = [AnatomyMaskPath '/' WhiteFile];
+    WhiteMatterTemplate = [AnatomyMaskPath '/' WhiteFile];
 end
 
 if ~isempty(CSFFile)
-  CSFTemplate         = [AnatomyMaskPath '/' CSFFile];
+    CSFTemplate         = [AnatomyMaskPath '/' CSFFile];
 end
 
 if (RunMode(1) | sum(RunMode) == 0)
     RunNamesTotal = RunDir;
     NumScanTotal  = NumScan;
-
+    
     MaskBrain = 1;
-
+    
     RegressGlobal    = any(strfind(upper(RegressOrder),'G'));
     RegressWhite     = any(strfind(upper(RegressOrder),'W'));
     RegressCSF       = any(strfind(upper(RegressOrder),'C'));
     DoBandpassFilter = any(strfind(upper(RegressOrder),'B'));
     RegressMotion    = any(strfind(upper(RegressOrder),'M'));
     DoLinearDetrend  = any(strfind(upper(RegressOrder),'D'));
-
+    
     for iSubject = 1:size(SubjDir,1)
         clear parameters;
         clear global SOM;
@@ -53,16 +53,16 @@ if (RunMode(1) | sum(RunMode) == 0)
         parameters.RegressFLAGS.csf      = RegressCSF;
         parameters.RegressFLAGS.white    = RegressWhite;
         parameters.RegressFLAGS.motion   = RegressMotion;
-        parameters.RegressFLAGS.order    = RegressOrder;  
-    
+        parameters.RegressFLAGS.order    = RegressOrder;
+        
         Subject=SubjDir{iSubject,1};
         RunList=SubjDir{iSubject,3};
-
+        
         SOM_LOG(sprintf('STATUS : Working on Subject : %s',Subject));
         NumRun = size(RunList,2);
-
+        
         TotalNumRun = size(NumScanTotal,2);  %%% number of image runs if every run were present
-
+        
         %%%%% This code cuts RunDir and NumScan based which Image Runs are present
         NumScan=[];
         clear RunDir;
@@ -70,70 +70,70 @@ if (RunMode(1) | sum(RunMode) == 0)
             RunDir{iRun,1} = RunNamesTotal{RunList(1,iRun)};
             NumScan        = horzcat(NumScan,NumScanTotal(1,RunList(1,iRun)));
         end
-
+        
         NumRun= size(NumScan,2); % number of runs
         ImageNumRun=size(RunDir,1); %number of image folders
-
-	if ~isempty(GreyFile)
-	  GreyPath  = mc_GenPath(GreyMatterTemplate);
-	end
-	if ~isempty(WhiteFile)
-	  WhitePath = mc_GenPath(WhiteMatterTemplate);
-	end
-	if ~isempty(CSFFile)
-	  CSFPath   = mc_GenPath(CSFTemplate);
-	end
+        
+        if ~isempty(GreyFile)
+            GreyPath  = mc_GenPath(GreyMatterTemplate);
+        end
+        if ~isempty(WhiteFile)
+            WhitePath = mc_GenPath(WhiteMatterTemplate);
+        end
+        if ~isempty(CSFFile)
+            CSFPath   = mc_GenPath(CSFTemplate);
+        end
         BrainPath = mc_GenPath(BrainMaskTemplate);
-	
-	if ~isempty(GreyFile)
-	  parameters.masks.grey.File          = GreyPath;
-	  parameters.masks.grey.ImgThreshold  = GreyThreshold;
-	end
-	
-	if ~isempty(WhiteFile)
-	  parameters.masks.white.File   = WhitePath;
-	end
-	if ~isempty(CSFFile)
-	  parameters.masks.csf.File     = CSFPath;
-	end
+        
+        if ~isempty(GreyFile)
+            parameters.masks.grey.File          = GreyPath;
+            parameters.masks.grey.ImgThreshold  = GreyThreshold;
+        end
+        
+        if ~isempty(WhiteFile)
+            parameters.masks.white.File   = WhitePath;
+        end
+        if ~isempty(CSFFile)
+            parameters.masks.csf.File     = CSFPath;
+        end
         parameters.masks.epi.File     = BrainPath;
         parameters.rois.mask.MaskFLAG = MaskBrain;
-
+        
         for iRun = 1:ImageNumRun
             Run = RunDir{iRun};
-
+            
             ImagePath   = mc_GenPath(ImageTemplate);
             ImageFiles  = spm_select('FPList',ImagePath, ['^' connectFile '.*.' imagetype]);
             parameters.data.run(iRun).P     = ImageFiles;
-	    if isempty(ImageFiles)
-	      SOM_LOG(sprintf('FATAL ERROR : P spm_select returned nothing for %s/^%s.*.%s',ImagePath,connectFile,imagetype));
-	      return
-	    end
-	    if ~isempty(confoundFile)
-	      cImageFiles = spm_select('FPList',ImagePath, ['^' confoundFile '.*.' imagetype]);
-	      if isempty(cImageFiles)
-		SOM_LOG(sprintf('FATAL ERROR : cP spm_select returned nothing for %s/^%s.*.%s',ImagePath,confoundFile,imagetype));
-		return
-	      end
-              parameters.data.run(iRun).cP  = cImageFiles;
-	    end
-	    
+            if isempty(ImageFiles)
+                SOM_LOG(sprintf('FATAL ERROR : P spm_select returned nothing for %s/^%s.*.%s',ImagePath,connectFile,imagetype));
+                return
+            end
+            if ~isempty(confoundFile)
+                cImageFiles = spm_select('FPList',ImagePath, ['^' confoundFile '.*.' imagetype]);
+                if isempty(cImageFiles)
+                    SOM_LOG(sprintf('FATAL ERROR : cP spm_select returned nothing for %s/^%s.*.%s',ImagePath,confoundFile,imagetype));
+                    return
+                end
+                parameters.data.run(iRun).cP  = cImageFiles;
+            end
+            
             RealignmentParametersFile = mc_GenPath(RealignmentParametersTemplate);
-
+            
             parameters.data.run(iRun).P = ImageFiles;
-
+            
             RealignmentParameters       = load(RealignmentParametersFile);
             RealignmentParametersDeriv  = diff(RealignmentParameters);
             RealignmentParametersDerivR = resample(RealignmentParametersDeriv,size(RealignmentParameters,1),size(RealignmentParametersDeriv,1));
-
+            
             parameters.data.run(iRun).MotionParameters = [RealignmentParameters RealignmentParametersDerivR];
             parameters.data.run(iRun).nTIME            = NumScan(iRun);
             
             %%%added code to handle censorVectors
-	    %
-	    % ROBERT DOES NOT RECOMMEND THE CURRENT IMPLEMENTATION OF CENSORING.
-	    % USE AT YOUR OWN STATISTICAL RISK
-	    %
+            %
+            % ROBERT DOES NOT RECOMMEND THE CURRENT IMPLEMENTATION OF CENSORING.
+            % USE AT YOUR OWN STATISTICAL RISK
+            %
             if (exist('CensorTemplate','var') && ~isempty(CensorTemplate))
                 CensorFile = mc_GenPath(struct('Template',CensorTemplate,'mode','check'));
                 [p f e] = fileparts(CensorFile);
@@ -152,7 +152,7 @@ if (RunMode(1) | sum(RunMode) == 0)
             %%%
             
             parameters.data.MaskFLAG = MaskBrain;
-
+            
             parameters.TIME.run(iRun).TR            = TR;
             parameters.TIME.run(iRun).BandFLAG      = DoBandpassFilter;
             parameters.TIME.run(iRun).TrendFLAG     = DoLinearDetrend;
@@ -163,11 +163,11 @@ if (RunMode(1) | sum(RunMode) == 0)
             parameters.TIME.run(iRun).whichFilter   = BandpassFilter;
             parameters.TIME.run(iRun).fraction      = Fraction;
         end
-
+        
         Run = RunDir{1};
         ImagePath = mc_GenPath(ImageTemplate);
         SOM_Mask  = mc_GenPath(fullfile(ImagePath,'som_mask.img'));
-
+        
         if (isempty(BrainMaskTemplate))
             parameters.rois.mask.File = SOM_Mask;
         else
@@ -201,6 +201,31 @@ if (RunMode(1) | sum(RunMode) == 0)
                     parameters.rois.mni.size.XROI = XYZ(1,:);
                     parameters.rois.mni.size.YROI = XYZ(2,:);
                     parameters.rois.mni.size.ZROI = XYZ(3,:);
+                end
+            case 'coordload'
+                tmpArray = load(mc_GenPath(ROIFile));
+                if isstruct(tmpArray)
+                    tmpArrayFields = fieldnames(tmpArray);
+                    %
+                    % Now we only expect one field, else throw an error
+                    %
+                    if length(tmpArrayField) ~= 1
+                        SOM_LOG(sprintf('FATAL ERROR : Ambiguous ROI information in file %s',mc_GenPath(ROIFile)));
+                        return
+                    end
+                    try
+                        parameters.rois.mni.coordinates = getfield(tmpArray,tmpArrayFields{1});
+                    catch
+                        SOM_LOG(sprintf('FATAL ERROR : Ambiguous ROI information in file %s',mc_GenPath(ROIFile)));
+                    end
+                    if (iscell(ROISize))
+                        parameters.rois.mni.size = ROISize{1};
+                    else
+                        XYZ = SOM_MakeSphereROI(ROISize);
+                        parameters.rois.mni.size.XROI = XYZ(1,:);
+                        parameters.rois.mni.size.YROI = XYZ(2,:);
+                        parameters.rois.mni.size.ZROI = XYZ(3,:);
+                    end
                 end
             case 'grid'
                 ROIGridMask     = mc_GenPath(ROIGridMaskTemplate);
@@ -238,9 +263,9 @@ if (RunMode(1) | sum(RunMode) == 0)
                     parameters.rois.mni.size.ZROI = XYZ(2,:);
                 end
         end
-
+        
         parameters.Output.correlation  = OutputType;
-	parameters.Output.power        = OutputPower;
+        parameters.Output.power        = OutputPower;
         parameters.Output.saveroiTC    = saveroiTC;
         %parameters.Output.description = 'description of output';
         parameters.Output.directory    = OutputPath;
@@ -263,7 +288,7 @@ if (RunMode(2))
         global SOM;
         SOM.silent = 1;
         SOM_LOG('STATUS : 01');
-
+        
         [D0 parameters] = SOM_PreProcessData(parameters);
         if D0 == -1
             SOM_LOG('FATAL ERROR : No data returned');
@@ -273,8 +298,13 @@ if (RunMode(2))
             if isnumeric(results)
                 SOM_LOG('FATAL ERROR : ');
                 mc_Error('There is something wrong with your template or your data.\nNo results were returned from SOM_CalculateCorrelations\n');
+            else
+                for iR = 1:size(results,1)
+                    SOM_LOG(results(iR,:))
+                end
+                SOM_LOG(sprintf('STATUS : Calculation done for Subject : %s',Subject));
             end
-        end        
+        end
     end
 end
 
--- ConnTool/ConnTool_batch_mc_template.m
+++ ConnTool/ConnTool_batch_mc_template.m
@@ -28,16 +28,16 @@ ImageTemplate = '[Exp]/Subjects/[Subject]/connect/func/[Run]/';
 %%% A list of run folders where the script can find functional images
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 RunDir = {
-	'run_01';
-};
+    'run_01';
+    };
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% The list of subjects to process
 %%% The format is 'subjectfolder',subject number in masterfile,[runs to include]
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 SubjDir = {
-'111129eb',1,[1];
-          };
+    '111129eb',1,[1];
+    };
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% The TR your data was collected at
@@ -46,9 +46,9 @@ TR = 2;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% The file to extract the CSF and WM confounds from
-%%% 
+%%%
 %%% It usually the run file that is in MNI space but prior
-%%% to smoothing. The idea is that you don't want gray 
+%%% to smoothing. The idea is that you don't want gray
 %%% smoothed into the CSF or WM regions.
 %%%
 %%% This can be the same file as the 'connectFile', but not ideal.
@@ -74,11 +74,11 @@ imagetype = 'nii';
 %%% (if you have more than 1 run, there should be more than 1 value here)
 %%%
 %%% If you have subjects with varying number of time points you can pick
-%%% the smallest, that will edit them down so they all have the 
+%%% the smallest, that will edit them down so they all have the
 %%% same effective statistical power.
 %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-NumScan = [240]; 
+NumScan = [240];
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -115,7 +115,7 @@ Mode = 'full';
 %%%
 %%%     CSFFile   --- name of the CSV image produced by VBM8
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-AnatomyMaskPath = '[Exp]/Subjects/[Subject]/connect/func/coRegRARUN/VBM8/'
+AnatomyMaskPath = '[Exp]/Subjects/[Subject]/connect/func/coRegRARUN/VBM8/';
 
 GreyFile  = [];
 WhiteFile = 'WM_ero*.nii';
@@ -149,21 +149,21 @@ RealignmentParametersTemplate = '[Exp]/Subjects/[Subject]/connect/func/[Run]/mcf
 %%%
 %%% Robert DOES NOT RECOMMEND CENSORING YOUR DATA.
 %%%
-%%%     This ConnTool Toolbox users an FFT filter with a little 
+%%%     This ConnTool Toolbox users an FFT filter with a little
 %%%     but of smoothing. If you want to censor you take the risk of
 %%%     created artifactual correlations across the whole brain
 %%%
 %%%     See the following three papers:
 %%%
-%%% 	Power JD, Barnes KA, Snyder AZ, Schlaggar BL, Petersen SE. 
-%%%     Spurious but Systematic Correlations in Functional Connectivity 
+%%% 	Power JD, Barnes KA, Snyder AZ, Schlaggar BL, Petersen SE.
+%%%     Spurious but Systematic Correlations in Functional Connectivity
 %%%     MRI Networks Arise From Subject Motion. NeuroImage 2012;59:2142–2154.
 %%%
-%%%	Carp J. Optimizing the Order of Operations for Movement Scrubbing: 
+%%%	Carp J. Optimizing the Order of Operations for Movement Scrubbing:
 %%%     Comment on Power Et Al. NeuroImage 2012:1–3.
 %%%
-%%% 	Power JD, Barnes KA, Snyder AZ, Schlaggar BL, Petersen SE. 
-%%%     Steps Toward Optimizing Motion Artifact Removal in Functional 
+%%% 	Power JD, Barnes KA, Snyder AZ, Schlaggar BL, Petersen SE.
+%%%     Steps Toward Optimizing Motion Artifact Removal in Functional
 %%%     Connectivity MRI; a Reply to Carp. NeuroImage 2012:1–3.
 %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -190,18 +190,18 @@ EPIThreshold   = [];
 %%%         C = csf
 %%%         M = motion
 %%%         B = bandpass
-%%% 
+%%%
 %%%         Suggested order is "DM[G]CWB"
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 RegressOrder = 'DMCWB';
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% The code users COMPCOR. 
+%%% The code users COMPCOR.
 %%% Use this many principle components for regression
 %%% for the CSF and WM
 %%%
-%%% Behzadi Y, Restom K, Liau J, Liu TT. 
-%%% A Component Based Noise Correction Method (CompCor) for BOLD and 
+%%% Behzadi Y, Restom K, Liau J, Liu TT.
+%%% A Component Based Noise Correction Method (CompCor) for BOLD and
 %%% Perfusion Based fMRI. NeuroImage 2007;37:90–101.
 %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -226,28 +226,47 @@ Fraction       = 1;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Type of input
 %%%         coordinates - provide the center of each seed and a radius
+%%%
+%%%         coordload   - load corridnate from the specified file
+%%%
 %%%         files       - provide a list of ROI files
+%%%
 %%%         directory   - provide a directory containing ROI files and the
-%%%                       script will load all images in that directory to 
+%%%                       script will load all images in that directory to
 %%%                       use as ROIs
+%%%
 %%%         grid        - make a grid based on provided spacing and masked
 %%%                       by provided mask
+%%%
 %%%         gridplus    - make a grid based on provided spacing and masked
 %%%                       by provided mask, as above.  Additionally, add
 %%%                       the extra ROI points specified in ROIGridCenters
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-ROIInput = 'coordinates';
+ROIInput = 'coordload';
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% If specifying ROI coordinates you need to provide a list of centers in 
+%%%
+%%% coordinates method
+%%%
+%%% If specifying ROI coordinates you need to provide a list of centers in
 %%% MNI coordinates (mm) and a radius in voxels.
-%%% NOTE: ROISize will be used as the radius of a sphere at each point. If 
-%%% you'd prefer to use the predefined 1,7,19, or 27 voxel sizes you will 
+%%% NOTE: ROISize will be used as the radius of a sphere at each point. If
+%%% you'd prefer to use the predefined 1,7,19, or 27 voxel sizes you will
 %%% need to specify the size as a cell (i.e. {19})
 %%%
 %%% See the MethodsCore/ConnTool/Documentation for more help on ROI size.
 %%%
-%%% You can load a file into the array ROICenters. 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+ROICenters = [0 -5 10];
+ROISize    = {19};
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%
+%%% coordload method
+%%%
+%%% You can load a file into the array ROICenters.
 %%%
 %%% If a ".csv" file you would do:
 %%%
@@ -255,14 +274,14 @@ ROIInput = 'coordinates';
 %%%
 %%% If a '.mat" file you need to load the file and the assign the variable.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-load(fullfile('/Volumes/ALS/ALS2008/matlabScripts/fcMRI/fcMRI_Network','V_MNI_12mmgrid.mat'));
-
-ROICenters = mni_coords;
-
+ROIFile = '[mcRoot]/ConnTool/Templates/V_MNI_12mmgrid.mat'
 ROISize = {19};
 
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%
+%%% files and directory method
+%%%
 %%% If specifying ROI images you need to provide an ROI folder as well as a
 %%% cell array list of ROI images.  If specifying an ROI directory, you only
 %%% need to specify an ROITemplate.  The script will then load all images
@@ -275,8 +294,11 @@ ROIImages = {
     };
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%
+%%% grid and gridplus method
+%%%
 %%% If specifying ROI grid you need to provide a spacing and ROI size as
-%%% well as an optional mask for grid point inclusion (a mask is strongly 
+%%% well as an optional mask for grid point inclusion (a mask is strongly
 %%% encouraged as not using one will return coordinates from across the entire
 %%% bounding box).
 %%% NOTE: ROIGridSize will be used as the radius of a sphere at each grid
@@ -288,6 +310,9 @@ ROIGridSize         = {19};
 ROIGridMaskTemplate = '[mcRoot]/ConnTool/Templates/symmetric_r3mm_EPI_MASK_NOEYES.nii';
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%
+%%% gridplus extra
+%%%
 %%% ROIGridCenters is used in 'gridplus' mode to specify additional ROIs
 %%% that you would like to include in addition to the regular grid.  They
 %%% will be added to the end of the list of ROIs and will use ROIGridSize
@@ -315,7 +340,7 @@ OutputType     = 'maps';
 %%%         correlation type can be 'full' or 'partial'
 %%%
 %%%         You can also save the power spectrum of the ROIS when running
-%%%         in 'maps' mode. This will only save the power spectrum of 
+%%%         in 'maps' mode. This will only save the power spectrum of
 %%%         single run.
 %%%            1 - save power spectrum
 %%%            0 - do not save power spectrum
