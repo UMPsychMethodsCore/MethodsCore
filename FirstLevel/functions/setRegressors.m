f2afe57d63055d16744ce24d99496cd754314764
Joseph Heffernan <heffjos@umich.edu>
2013-06-14 15:33:14 -0400

    Paritioned the central code of setRegressors to separate function to handle the different stages of handling regressor files.

--- FirstLevel/functions/setRegressors.m
+++ FirstLevel/functions/setRegressors.m
@@ -67,176 +67,68 @@ function subjectInfo = setRegressors(opt, subjectInfo)
         for k = 1:NumRuns
 
             Run = opt.RunDir{ opt.SubjDir{i, 3}(k), 1 };
+            RunRegressors = [];
 
-            for iRegFile = 1:size(opt.RegFilesTemplate)
+            for iRegFile = 1:size(opt.RegFilesTemplate, 1)
 
-                % RegMatrix = readRegFile(Exp, Subject, Run, RegFile)
-                RegFileCheck.Template = opt.RegFilesTemplate{iRegFile, 1};
-                RegFileCheck.mode = 'check';
-                RegFile = mc_GenPath(RegFileCheck);
-                RegMatrix = load(RegFile);
-
+                % get files
+                RegFile = opt.RegFilesTemplate{iRegFile, 1};
+                RegMatrix = readRegFile(Exp, Subject, Run, RegFile);
                 if isempty(RegMatrix) == 1
-                    msg = sprintf(['Regessor file %s\n' ...
-                                   ' has no regressors for subject %s run %s\n\n'], ...
-                                   RegFile, Subject, Run);
-                    fprintf(1, msg);
-                    mc_Logger('log', msg, 2);
                     subjectInfo(i).sess(k).useRegress = false;
                     continue;
                 end
 
-                % trim regressors if needed
-                % RegMatrix = trimRegMatrix(RegMatrix, RegNumCol) ~ continue
-                RegNumCol = opt.RegFilesTemplate{iRegFile, 2};
-                if ~isinf(RegNumCol) == 1
-
-                    % check if number to keep is bigger then present
-                    if RegNumCol > size(RegMatrix, 2)
-                        msg = sprintf(['Regressor file %s\n' ...
-                                       ' has %d regressors is less than %d regressors to keep.'...
-                                       '  Keeping all regressors present.\n\n'], ...
-                                       RegFile, size(RegMatrix, 2), RegNumCol);
-                        fprintf(1, msg);
-                        mc_Logger('log', msg, 2);
-                    else
-                        RegMatrix = RegMatrix(:, 1:RegNumCol);
-                    end
-
-                    % handle if all regressors are removed
-                    if isempty(RegMatrix) == 1
-                        msg = sprintf(['Regressor file %s\n' ...
-                                       ' lost all regresors after trimming for subject %s' ...
-                                       ' run %s\n\n'], RegFile, Subject, Run);
-                        fprintf(1, msg);
-                        mc_Logger('log', msg, 2);
-                        subjectInfo(i).sess(k).useRegress = false;
-                        continue;
-                    end
+                % trim regressors
+                ColumnsToKeep = opt.RegFilesTemplate{iRegFile, 2};
+                RegMatrix = trimRegressor(Subject, Run, RegFile, ColumnsToKeep, RegMatrix);
+                if isempty(RegMatrix)
+                    subjectInfo(i).sess(k).useRegress = false;
+                    continue;
                 end
-
+                    
                 % remove constant regressors
-                % RegMatrix = removeConstRegressors(RegMatrix)
-                NumReg = size(RegMatrix, 2);
-                RegColToRemove = [];
-                for iRegMatrix = 1:NumReg
-                    if all( RegMatrix(:, iRegMatrix) == RegMatrix(1, iRegMatrix) )
-                        RegColToRemove(end+1) = iRegMatrix;
-                    end
+                RegMatrix = removeConstantColumns(Subject, Run, RegFile, RegMatrix);
+                if isempty(RegMatrix)
+                    subjectInfo(i).sess(k).useRegress = false;
+                    continue;
                 end
 
-                if ~isempty(RegColToRemove) == 1
-
-                    RegMatrix(:, RegColToRemove) = [];
-
-                    if isempty(RegMatrix)
-                        msg = sprintf(['Regressor file %s\n' ...
-                                       ' had all constant regressors.  Subject %s run %s' ...
-                                       ' will not be using any regressors.\n\n'], ...
-                                       RegFile, Subject, Run);
-                        fprintf(1, msg);
-                        mc_Logger('log', msg, 2);
-                        subjectInfo(i).sess(k).useRegress = false;
-                        continue;
-                    else
-                        msg = sprintf(['%s\n' ...
-                                       '   Found %d regressors and removed %d constant' ...
-                                       ' regressors for subject %s in run %s\n'], ...
-                                       NumReg, length(RegColToRemove), Subject, Run);
-                        fprintf(1, mgs);
-                        mc_Logger('log', msg, 3);
-                    end
-                end
-                        
                 % calculate and add derivatives if need be
-                % RegMatrix = addDerivatives(RegMatrix, Order, ConstantColumsRemoved)
-                doDerivativeFlag = true;
-                RegDerivOrder = opt.RegFilesTemplate{iRegFile, 3};
-                if RegDerivOrder > size(RegMatrix, 1) 
-                    if length(RegColToRemove) ~= NumReg
-                        error(['ERROR: Invalid derivative order for subject %s run %s.\n' ...
-                               '       Using regressor file %s\n'], Subject, Run, RegFile);
-                    else
-                        msg = sprintf(['Cannot add derivatives for regressor file %s\n' ...
-                                       'All regressors were constant and thus removed.\n'], ...
-                                       RegFile);
-                        fprintf(1, msg);
-                        mc_Logger('log', msg, 3);
-                        doDerivativeFlag = false;
-                    end
+                Order = opt.RegFilesTemplate{iRegFile, 3};
+                RegMatrix = addDerivatives(Subject, Run, RegFile, Order, RegMatrix);
+
+                try
+                    RunRegressors = [RunRegressors RegMatrix];
+                catch
+                    error(['RegFile %s does not have the same number of columns as the other' ...
+                           ' regressors for subject %s run %s'], ...
+                           RegFile, Subject, Run);
                 end
 
-                % finally add derivates if good to go
-                if RegDerivOrder > 0 && doDerivativeFlag
-
-                    NumVolumes = size(RegMatrix, 1);
-
-                    tmpRegMatrix = RegMatrix;
-                    DerivativesIncluded = 0;
-                    LastDerivEndCol = size(RegMatrix, 2);
-
-                    for iDeriv = 1:RegDerivOrder
-
-                        for iRegMatrix = 1:size(tmpRegMatrix, 2)
-
-                            tmpDeriv = diff( tmpRegMatrix(:, iRegMatrix) );
-                            tmpDeriv = resample(tmpDeriv, NumVolumes, length(tmpDeriv));
-
-                            % check to make sure derivative is not a constant now
-                            if ~all( tmpDeriv(1) == tmpDeriv )
-                                
-                                RegMatrix(:, end+1) = tmpDeriv;
-                                DerivativesIncluded = DerivativesIncluded + 1;
-
-                            else
-
-                                msg = sprintf(['RegFile %s\n' ...
-                                               ' had a constant derivative for %dth' ...
-                                               ' derivative. Not including it or any' ...
-                                               ' higher derivatives for regressor number %d\n\n'], ...
-                                                RegFile, iDeriv, iRegMatrix);
-                                fprintf(1, msg);
-                                mc_Logger('log', msg, 2);
-
-                            end
-
-                        end
-
-                        if DerivativesIncluded == 0
-
-                            msg = sprintf(['Regfile %s\n' ...
-                                           ' had all constant derivatives for order %d' ...
-                                           ' Only including derivatives up to the %dth order'], ...
-                                           RegFile, iDeriv, iDeriv - 1);
-                            fprintf(1, msg);
-                            mc_Logger('log', msg, 2);
-                            break;
-
-                        else
-
-                            % now update variables
-                            tmpRegMatrix = RegMatrix(:, LastDerivEndCol+1:end);
-                            DerivativesIncluded = 0;
-                            LastDerivEndCol = size(RegMatrix, 2);
-
-                        end
-                    end
-                end
-
-            % end of RegFileTemplate loop
             end
             
             % now we need to save to subjectInfo
-            for iRegMatrix = 1:size(RegMatrix, 2)
+            if isempty(RunRegressors) == 0
 
-                RegName = sprintf('R%03d', iRegMatrix);
-                subjectInfo(i).sess(k).regress(iRegMatrix).val = RegMatrix(:, iRegMatrix);
-                subjectInfo(i).sess(k).regress(iRegMatrix).name = RegName;
+                for iRegMatrix = 1:size(RegMatrix, 2)
+
+                    RegName = sprintf('R%03d', iRegMatrix);
+                    subjectInfo(i).sess(k).regress(iRegMatrix).val = RunRegressors(:, iRegMatrix);
+                    subjectInfo(i).sess(k).regress(iRegMatrix).name = RegName;
+
+                end
+                
+                subjectInfo(i).sess(k).useRegress = true;
+            else
+
+                msg = sprintf('All regressor files were trimmed to nothing for subject %s run %s', Subjec, Run);
+                fprintf(msg);
+                mc_Logger('log', msg, 2);
+                subjectInfo(i).sess(k).useRegress = false;
 
             end
             
-%            subject
-
             % end of run loop
         end
 
@@ -248,7 +140,7 @@ end
 
 function RegMatrix = readRegFile(Exp, Subject, Run, RegFile)
 
-    RegFileCheck.Template = opt.RegFilesTemplate{iRegFile, 1};
+    RegFileCheck.Template = RegFile;
     RegFileCheck.mode = 'check';
     RegFile = mc_GenPath(RegFileCheck);
     RegMatrix = load(RegFile);
@@ -259,7 +151,131 @@ function RegMatrix = readRegFile(Exp, Subject, Run, RegFile)
                        RegFile, Subject, Run);
         fprintf(1, msg);
         mc_Logger('log', msg, 2);
-        subjectInfo(i).sess(k).useRegress = false;
-        continue;
+    end
+
+end
+
+function RegMatrix = trimRegressor(Subject, Run, RegFile, ColumnsToKeep, RegMatrix)
+
+    if ~isinf(ColumnsToKeep) == 1
+    
+        % check if number to keep is bigger then present
+        if ColumnsToKeep > size(RegMatrix, 2)
+            msg = sprintf(['Regressor file %s\n' ...
+                           ' has %d regressors is less than %d regressors to keep.'...
+                           '  Keeping all regressors present.\n\n'], ...
+                           RegFile, size(RegMatrix, 2), ColumnsToKeep);
+            fprintf(1, msg);
+            mc_Logger('log', msg, 2);
+        else
+            RegMatrix = RegMatrix(:, 1:RegNumCol);
+        end
+    
+        % handle if all regressors are removed
+        if isempty(RegMatrix) == 1
+            msg = sprintf(['Regressor file %s\n' ...
+                           ' lost all regresors after trimming for subject %s' ...
+                           ' run %s\n\n'], RegFile, Subject, Run);
+            fprintf(1, msg);
+            mc_Logger('log', msg, 2);
+        end
+    end
+
+end
+
+function RegMatrix = removeConstantColumns(Subject, Run, RegFile, RegMatrix)
+
+    NumReg = size(RegMatrix, 2);
+    RegColToRemove = [];
+    for iRegMatrix = 1:NumReg
+        if all( RegMatrix(:, iRegMatrix) == RegMatrix(1, iRegMatrix) )
+            RegColToRemove(end+1) = iRegMatrix;
+        end
+    end
+    
+    if ~isempty(RegColToRemove) == 1
+    
+        RegMatrix(:, RegColToRemove) = [];
+    
+        if isempty(RegMatrix)
+            msg = sprintf(['Regressor file %s\n' ...
+                           ' had all constant regressors.  Subject %s run %s' ...
+                           ' will not be using any regressors.\n\n'], ...
+                           RegFile, Subject, Run);
+            fprintf(1, msg);
+            mc_Logger('log', msg, 2);
+        else
+            msg = sprintf(['%s\n' ...
+                           '   Found %d regressors and removed %d constant' ...
+                           ' regressors for subject %s in run %s\n'], ...
+                           NumReg, length(RegColToRemove), Subject, Run);
+            fprintf(1, msg);
+            mc_Logger('log', msg, 3);
+        end
+    end
+
+end
+
+function RegMatrix = addDerivatives(Subject, Run, RegFile, Order, RegMatrix)
+
+    if Order > size(RegMatrix, 1) 
+        error(['ERROR: Invalid derivative order for subject %s run %s.\n' ...
+               '       Using regressor file %s\n'], Subject, Run, RegFile);
+    end
+    
+    if Order > 0
+    
+        NumVolumes = size(RegMatrix, 1);
+    
+        tmpRegMatrix = RegMatrix;
+        DerivativesIncluded = 0;
+        LastDerivEndCol = size(RegMatrix, 2);
+    
+        for iDeriv = 1:Order
+    
+            for iRegMatrix = 1:size(tmpRegMatrix, 2)
+    
+                tmpDeriv = diff( tmpRegMatrix(:, iRegMatrix) );
+                tmpDeriv = resample(tmpDeriv, NumVolumes, length(tmpDeriv));
+    
+                % check to make sure derivative is not a constant now
+                if ~all( tmpDeriv(1) == tmpDeriv )
+                    
+                    RegMatrix(:, end+1) = tmpDeriv;
+                    DerivativesIncluded = DerivativesIncluded + 1;
+    
+                else
+    
+                    msg = sprintf(['RegFile %s\n' ...
+                                   ' had a constant derivative for %dth' ...
+                                   ' derivative. Not including it or any' ...
+                                   ' higher derivatives for regressor number %d\n\n'], ...
+                                    RegFile, iDeriv, iRegMatrix);
+                    fprintf(1, msg);
+                    mc_Logger('log', msg, 2);
+    
+                end
+    
+            end
+    
+            if DerivativesIncluded == 0
+    
+                msg = sprintf(['Regfile %s\n' ...
+                               ' had all constant derivatives for order %d' ...
+                               ' Only including derivatives up to the %dth order'], ...
+                               RegFile, iDeriv, iDeriv - 1);
+                fprintf(1, msg);
+                mc_Logger('log', msg, 2);
+                break;
+    
+            else
+    
+                % now update variables
+                tmpRegMatrix = RegMatrix(:, LastDerivEndCol+1:end);
+                DerivativesIncluded = 0;
+                LastDerivEndCol = size(RegMatrix, 2);
+    
+            end
+        end
     end
 end
