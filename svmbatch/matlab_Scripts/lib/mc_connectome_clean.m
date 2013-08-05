05be3bc040ffe101c1c5ecae207934a74c4d9848
Daniel A Kessler <kesslerd@umich.edu>
2013-08-05 12:12:02 -0400

    Tweak mc_connectome_clean to add two new features
    1) ability to not treat 0's as "bad" data. This was problematic
    for a lot of our statistical modeling, where really big fx had p values
    that rounded to 0, and mc_connectome_clean was throwing those out.
    
    2) Ability to replace "bad" data with an optional scalar (or Inf, NaN, etc).
    By default it is 0, but if 0's are not being treated as bad data (see above)
    it defaults to 1

--- svmbatch/matlab_Scripts/lib/mc_connectome_clean.m
+++ svmbatch/matlab_Scripts/lib/mc_connectome_clean.m
@@ -1,4 +1,4 @@
-function [ data_clean, censor ] = mc_connectome_clean( data_in )
+function [ data_clean, censor ] = mc_connectome_clean( data_in ,cleanzeros,badval)
 %MC_CONNECTOME_CLEAN This routine will make sure all of your loaded featres
 %are valid. This will work for both 3D (multi condition, paired data) or 2D
 %(multiclass) connectome cases.
@@ -11,6 +11,15 @@ function [ data_clean, censor ] = mc_connectome_clean( data_in )
 %       data_in     -   Should either be a 2D or 3D object returned by call
 %                       to mc_load_connectomes_{unpaired,paired},
 %                       respectively
+%
+%       cleanzeros  -   Set me to true if you want me to treat zeros as "bad" data.
+%                       If set to true, I will return 0's in place of bad data.
+%                       If set to false, I will return NaNs in place of bad data
+%                       Defaults to true
+%
+%       badval      -   What should I replace "bad" values with? Default is 0.
+%                       If cleanzeros is false, this will default to NaN.
+%                       Probably a good idea to manually set this, though
 % 
 %   OUTPUT
 %       data_clean  -   A censored version of data_in. Should have the same
@@ -18,27 +27,53 @@ function [ data_clean, censor ] = mc_connectome_clean( data_in )
 %       censor      -   A sparse 1 * nFeat matrix, showing what was
 %                       censored. 1 indicates positive censorship.
 
+%% Parse optional arguments
+
+if ~exist('cleanzeros','var')
+    cleanzeros=true;
+end
+
+if ~exist('badval','var') % if badval is undefined, set default
+                          % depending on clean zeros
+    if ~cleanzeros % if cleanzeros set to false, set badval to NaN
+        badval = NaN;
+    elseif cleanzeros % if cleanzeros set to true, set badval to 0
+        badval=0;
+    end
+end
+
+%% Do work
+
 switch ndims(data_in)
     
     case 2 % 2D data_in
         censor_nan=sparse(any(isnan(data_in))); % ID any NaN's
         censor_inf=sparse(any(isinf(data_in))); % ID any Inf's
         censor_zed=sparse(any(data_in==0)); % ID any zero values
-        
-        censor=sparse(any([censor_nan; censor_inf; censor_zed])); % find union of DQ's
+        if exist('cleanzeros','var') && ~cleanzeros
+            censor=sparse(any([censor_nan; censor_inf])); % find union of DQ's
+        else
+            censor=sparse(any([censor_nan; censor_inf; censor_zed])); % find union of DQ's            
+        end
         
         data_clean=data_in;
-        data_clean(:,logical(censor))=0; % zero out all censored elements
-        
+
+        data_clean(:,logical(censor)) = badval;
+
+
     case 3  % 3D data_in
         censor_nan=sparse(any(any(isnan(data_in)),3)); % ID any NaN's
         censor_inf=sparse(any(any(isinf(data_in)),3)); % ID any Inf's
         censor_zed=sparse(any(any(data_in==0),3)); % ID any zero values
-        
-        censor=sparse(any([censor_nan; censor_inf; censor_zed])); % find union of DQ's
+
+        if exist('cleanzeros','var') && ~cleanzeros
+            censor=sparse(any([censor_nan; censor_inf])); % find union of DQ's
+        else
+            censor=sparse(any([censor_nan; censor_inf; censor_zed])); % find union of DQ's
+        end
         
         data_clean=data_in;
-        data_clean(:,logical(censor),:)=0; % zero out all censored elements at all conditions
-        
+        data_clean(:,logical(censor),:)=badval; % replace bad data 
+
 end
 
