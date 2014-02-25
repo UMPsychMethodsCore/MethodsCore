27bdb4ca02f0670d6f63265bd3155287c8437888
Daniel A Kessler <kesslerd@umich.edu>
2014-02-25 15:12:43 -0500

    add batch utilities for BNV. These were just copy/pasted from svmbatch_alpha, and it makes more sense for them to be here

---
+++ Utilities/BrainNetViewer/Batch/BrainNet_central.m
@@ -0,0 +1,15 @@
+if strcmp(BrainVol,'')
+    BrainVol = fullfile(mcRoot,'svmbatch','lib','BrainNetViewer','Data','SurfTemplate','BrainMesh_Ch2withCerebellum.nv');
+end
+
+if strcmp(CfgFile,'')
+    CfgFile = fullfile(mcRoot,'svmbatch','lib','BrainNetViewer','Data','Brain_AAL_Nodes_Edges_edited.mat');
+end
+
+for iC = 1:size(Files,1)
+    
+    NodeFile = fullfile(Exp,[Files{iC} '.node']);
+    EdgeFile = fullfile(Exp,[Files{iC} '.edge']);
+    OutputPath = fullfile(Exp,[Files{iC} '.bmp']);
+    BrainNet_MapCfg(BrainVol,NodeFile,EdgeFile,CfgFile,OutputPath,ind);
+end
