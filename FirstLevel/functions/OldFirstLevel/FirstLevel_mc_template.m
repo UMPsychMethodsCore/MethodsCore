1e1d864ab8539e4389a060aa200d37934dfc4c36
Joseph Heffernan <heffjos@umich.edu>
2014-02-27 14:19:23 -0500

    Updated added paths on legacy first level template.m

--- FirstLevel/functions/OldFirstLevel/FirstLevel_mc_template.m
+++ FirstLevel/functions/OldFirstLevel/FirstLevel_mc_template.m
@@ -397,7 +397,7 @@ mcRoot = fullfile(fileparts(mfilename('fullpath')),'..');
 %[DEVmcRootAssign]
 
 addpath(fullfile(mcRoot,'matlabScripts'))
-addpath(fullfile(mcRoot,'FirstLevel'))
+addpath(fullfile(mcRoot,'FirstLevel/functions/OldFirstLevel'))
 addpath(fullfile(mcRoot,'SPM','SPM8','spm8_with_R4667'))
 
-FirstLevel_mc_central
+OldFirstLevel_mc_central
